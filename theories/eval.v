48e2207f98aac0873e6313a2c6dd1c0ac06f407d
François Pottier <francois.pottier@inria.fr>
2023-02-15 23:20:37 +0100

    Cleanup and comments.

--- theories/eval.v
+++ theories/eval.v
@@ -24,6 +24,9 @@ Implicit Type η : env.
              this effect can be viewed of consulting an oracle,
              which answers a request with a value.
 
+   The definition of the type [mon A] is inductive:
+   every computation must terminate.
+
    The requests allowed by this monad are:
 
    - [REval η e], a request to evaluate the expression [e]
@@ -47,24 +50,51 @@ Arguments Stop {A} req k.
 
 (* ------------------------------------------------------------------------ *)
 
-(* TODO if call-by-value evaluation is used
-   then [g] should have type [unit → mon B]
-   to avoid evaluating all branches in a [match] construct *)
+(* Monadic combinators. *)
 
-Fixpoint try {A B : Type} (m : mon A) (f : A → mon B) (g : mon B) : mon B :=
+(* [try m f g] runs the computation [m]. If [m] returns a result [v], then
+   [f v] is executed. If [m] ends with a soft failure [Next], then [g()] is
+   executed. *)
+
+(* [g] must have type [unit → mon B], as opposed to just [mon B], because
+   we execute monadic computations (in Coq) using call-by-value evaluation
+   and we do not want to evaluate ALL branches in a [match] construct. *)
+
+Fixpoint try {A B} (m : mon A) (f : A → mon B) (g : unit → mon B) : mon B :=
   match m with
   | Ret a =>
       f a
   | Fail =>
+      (* A hard failure is transmitted. *)
       Fail
   | Next =>
-      g
+      g()
   | Stop req k =>
+      (* An effect is transmitted. The combinator [try _ f g] remains
+         installed on top of the continuation. *)
       Stop req (λ v, try (k v) f g)
   end.
 
-Definition bind {A B : Type} (m : mon A) (f : A → mon B) : mon B :=
-  try m f Next.
+(* [bind m f] sequences the computations [m] and [f]. *)
+
+(* [bind] is a special case of [try]. If [m] ends with a soft failure,
+   it is transmitted. *)
+
+Definition bind {A B} (m : mon A) (f : A → mon B) : mon B :=
+  try m f (λ tt, Next).
+
+(* ------------------------------------------------------------------------ *)
+
+(* Equality of monadic computations. *)
+
+(* This equality is needed to state some of the monad laws. That said, it
+   may be the case that we actually do not need these laws. *)
+
+(* This equality is just equality of trees whose internal nodes are the
+   [Stop] nodes and whose leaves are [Ret], [Fail] and [Next]. *)
+
+(* If the axiom of functional extensionality is accepted, then [eq]
+   coincides with Coq's ordinary notion of equality. *)
 
 Inductive eq {A} : mon A → mon A → Prop :=
 | EqRet a :
@@ -78,37 +108,46 @@ Inductive eq {A} : mon A → mon A → Prop :=
     eq (Stop req k1) (Stop req k2)
 .
 
+Local Hint Constructors eq : eq.
+
+Infix "~" := eq (at level 70, no associativity).
+
+(* Equality is reflexive and transitive. *)
+
 Lemma eq_reflexive {A} (m : mon A) :
-  eq m m.
+  m ~ m.
 Proof.
   induction m; constructor; eauto.
 Qed.
 
-Lemma mon_law1 {A B : Type} (a : A) (f : A → mon B) :
-  bind (Ret a) f = f a.
+Lemma eq_transitive {A} (m1 m2 : mon A) :
+  m1 ~ m2 → ∀ m3, m2 ~ m3 → m1 ~ m3.
 Proof.
-  reflexivity.
+  induction 1; inversion 1; subst; constructor; eauto.
 Qed.
 
-Lemma mon_law2 {A B : Type} (m : mon A) :
-  eq
-    (bind m Ret)
-    m.
+Local Hint Resolve eq_reflexive : eq.
+
+(* The monadic laws. *)
+
+Lemma monad_law_left_unit {A B} (a : A) (f : A → mon B) :
+  bind (Ret a) f ~ f a.
+Proof.
+  apply eq_reflexive.
+Qed.
+
+Lemma monad_law_right_unit {A} (m : mon A) :
+  bind m Ret ~ m.
 Proof.
   unfold bind. induction m; simpl try; constructor; eauto.
 Qed.
 
-Lemma mon_law3 {A B C} (m : mon A) (g : A → mon B) (h : B → mon C) :
-  eq
-    (bind (bind m g) h)
-    (bind m (λ a, bind (g a) h)).
+Lemma monad_law_associativity
+  {A B C} (m : mon A) (g : A → mon B) (h : B → mon C) :
+  bind (bind m g) h ~
+  bind m (λ a, bind (g a) h).
 Proof.
-  unfold bind.
-  induction m; simpl.
-  { apply eq_reflexive. }
-  { constructor. }
-  { constructor. }
-  { constructor. eauto. }
+  unfold bind. induction m; simpl; eauto with eq.
 Qed.
 
 (* Evaluation. *)
@@ -200,7 +239,7 @@ with eval_branches η v bs :=
         (* Success: commit to this branch. Evaluate its body. *)
         (λ η, eval η e)
         (* Soft failure: abandon this branch. Try the following branches. *)
-        (eval_branches η v bs)
+        (λ tt, eval_branches η v bs)
   end.
 
 (* A reduction semantics on top of the above evaluator. *)
