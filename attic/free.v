3e9551609a94033d504d6db9f28d9ac4b035aba3
François Pottier <francois.pottier@inria.fr>
2023-02-16 22:00:24 +0100

    Move unused definitions to attic/free.v.

---
+++ attic/free.v
@@ -0,0 +1,33 @@
+(* Equality on computations in the free monad. *)
+
+Inductive eq {A} : mon A → mon A → Prop :=
+| EqRet a :
+    eq (Ret a) (Ret a)
+| EqFail :
+    eq Fail Fail
+| EqNext :
+    eq Next Next
+| EqStop req k1 k2 :
+    (∀ v, eq (k1 v) (k2 v)) →
+    eq (Stop req k1) (Stop req k2)
+.
+
+Local Hint Constructors eq : eq.
+
+Local Infix "~" := eq (at level 70, no associativity).
+
+(* Equality is reflexive and transitive. *)
+
+Lemma eq_reflexive {A} (m : mon A) :
+  m ~ m.
+Proof.
+  induction m; constructor; eauto.
+Qed.
+
+Lemma eq_transitive {A} (m1 m2 : mon A) :
+  m1 ~ m2 → ∀ m3, m2 ~ m3 → m1 ~ m3.
+Proof.
+  induction 1; inversion 1; subst; constructor; eauto.
+Qed.
+
+Local Hint Resolve eq_reflexive : eq.
