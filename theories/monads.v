c649cedf1be7521eb15709cf8c271d4c4a240f2f
François Pottier <francois.pottier@inria.fr>
2023-02-18 22:54:47 +0100

    Cleanup in monads.v.

--- theories/monads.v
+++ theories/monads.v
@@ -1,29 +1,34 @@
 From ExtLib.Structures Require Export Monads MonadLaws.
 
+Section Laws.
+
+Context {m : Type -> Type}.
+
 (* The type class MonadFix in coq-ext-lib does not work for us,
    so we replace it with the following class. *)
 
-Section MonadFixLaws.
+Class MonadFixLaws (MF : MonadFix m) := {
+  mleq :
+    forall {A}, m A -> m A -> Prop;
+  mleq_reflexive :
+    forall {A} (m : m A),
+    mleq m m;
+  mleq_transitive :
+    forall {A} (m1 m2 m3 : m A),
+    mleq m1 m2 -> mleq m2 m3 -> mleq m1 m3;
+  mfix_fixed_point :
+    forall {T A} (ff : (T -> m A) -> (T -> m A)),
+    (forall p1 p2,
+      (forall t, mleq (p1 t) (p2 t)) ->
+      (forall t, mleq (ff p1 t) (ff p2 t))) ->
+    mfix ff = ff (mfix ff)
+}.
 
-  Context {m : Type -> Type}.
-  Context (MF : MonadFix m).
+Class MonadFixCoinduction (MF : MonadFix m) (MFL : MonadFixLaws MF) := {
+  mfix_coinduction :
+    forall {T A} (ff : (T -> m A) -> (T -> m A)) (p : T -> m A),
+    (forall t, mleq (p t) (ff p t)) ->
+    (forall t, mleq (p t) (mfix ff t))
+}.
 
-  Class MonadFixLaws :=
-    {
-      mleq :
-        forall {A}, m A -> m A -> Prop;
-      mleq_reflexive :
-        forall {A} (m : m A),
-        mleq m m;
-      mleq_transitive :
-        forall {A} (m1 m2 m3 : m A),
-        mleq m1 m2 -> mleq m2 m3 -> mleq m1 m3;
-      mfix_fixed_point :
-        forall {T A} (ff : (T -> m A) -> (T -> m A)),
-        (forall p1 p2,
-          (forall t, mleq (p1 t) (p2 t)) ->
-          (forall t, mleq (ff p1 t) (ff p2 t))) ->
-        mfix ff = ff (mfix ff)
-    }.
-
-End MonadFixLaws.
+End Laws.
