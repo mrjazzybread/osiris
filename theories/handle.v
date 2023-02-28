de5a720fffba7581c57676b7a6cb85b4f218c8a8
François Pottier <francois.pottier@inria.fr>
2023-02-28 19:53:42 +0100

    Update [handle] to use [iter] instead of [mfix].

--- theories/handle.v
+++ theories/handle.v
@@ -11,9 +11,9 @@ Context {M : Monad target}.
 Context {ML : MonadLaws M}.
 Context {MZ : MonadZero target}.
 Context {MZL : MonadZeroLaws M MZ}.
-Context {MF : MonadFix target}.
-Context {MFL : MonadFixLaws MF}.
-Context {MFC : MonadFixCoinduction MF MFL}.
+Context {MI : MonadIter target}.
+Context {MS : MonadSkip target}.
+Context {MIL : MonadIterLaws M MI MS}.
 
 Section A.
 
@@ -23,43 +23,39 @@ Context {A : Type}.
 
 (* An interpretation of our free monad into the target monad. *)
 
-Definition handle_pre
-  (handle : free.mon A → target A)
-  (m : free.mon A) : target A
-:=
+Definition handle_body (m : free.mon A) : target (free.mon A + A) :=
   match m with
   | free.Ret a =>
       (* Termination is mapped to termination. *)
-      ret a
+      ret (inr a)
   | free.Fail =>
       (* Hard failure is mapped to hard failure. *)
       mzero
   | free.Next =>
       (* Soft failure is not expected to happen. *)
       mzero
-  | free.Stop req k =>
+  | free.Stop (free.REval η e) k =>
       (* A [Stop] effect is mapped to an invocation of [eval]. The call
          [eval η e] is composed with the continuation [k]. This computation
-         is (recursively) transported by [handle] from the free monad into
-         the target monad. *)
-      match req with
-      | free.REval η e =>
-          handle (bind (eval η e) k)
-      end
+         is then transported from the free monad into the target monad via
+         an (implicit) tail recursive call to [handle]. *)
+      ret (inl (bind (eval η e) k))
   end.
 
 Definition handle : free.mon A → target A :=
-  mfix handle_pre.
+  iter handle_body.
 
-Lemma handle_fixed_point :
-  handle = handle_pre handle.
+Lemma handle_fixed_point (m : free.mon A) :
+  handle m =
+    bind (handle_body m) (fun (signal : free.mon A + A) =>
+      match signal with
+      | inl m =>
+          skip (handle m)
+      | inr r =>
+          ret r
+      end).
 Proof.
-  eapply mfix_fixed_point.
-  (* We must check that [handle_pre] is monotonic. *)
-  intros self1 self2 Hself m.
-  destruct m; simpl;
-  eauto using mleq_reflexive.
-  destruct req; eauto.
+  eapply unfold_iter. eauto.
 Qed.
 
 (* Paraphrase lemmas. *)
@@ -67,26 +63,33 @@ Qed.
 Lemma handle_ret (a : A) :
   handle (free.Ret a) = ret a.
 Proof.
-  rewrite handle_fixed_point. reflexivity.
+  rewrite handle_fixed_point. unfold handle_body.
+  rewrite bind_of_return by typeclasses eauto.
+  reflexivity.
 Qed.
 
 Lemma handle_fail :
   handle (free.Fail : free.mon A) = mzero.
 Proof.
-  rewrite handle_fixed_point. reflexivity.
+  rewrite handle_fixed_point. unfold handle_body.
+  rewrite bind_zero by typeclasses eauto.
+  reflexivity.
 Qed.
 
 Lemma handle_next :
   handle (free.Next : free.mon A) = mzero.
 Proof.
-  rewrite handle_fixed_point. reflexivity.
+  rewrite handle_fixed_point. unfold handle_body.
+  rewrite bind_zero by typeclasses eauto.
+  reflexivity.
 Qed.
 
 Lemma handle_stop η e (k : val → free.mon A) :
-  handle (free.Stop (free.REval η e) k) = handle (bind (eval η e) k).
+  handle (free.Stop (free.REval η e) k) =
+  skip (handle (bind (eval η e) k)).
 Proof.
-  pattern handle at 1; rewrite handle_fixed_point.
-  simpl.
+  rewrite (handle_fixed_point (free.Stop _ _)). unfold handle_body.
+  rewrite bind_of_return by typeclasses eauto.
   reflexivity.
 Qed.
 
@@ -108,24 +111,24 @@ Definition run η e : target val :=
 
 Lemma compatibility {A B} (m : free.mon A) :
   ∀ (f : A → free.mon B),
-  mleq
-    (bind (handle m) (λ v, handle (f v)))
-    (handle (bind m f)).
+  bind (handle m) (λ v, handle (f v)) =
+  handle (bind m f).
 Proof.
   induction m as [ | | | [ η e] k IH ]; intros.
   { rewrite handle_ret.
     do 2 rewrite bind_of_return by typeclasses eauto.
-    eapply mleq_reflexive. }
+    reflexivity. }
   { rewrite free.free_bind_fail.
     do 2 rewrite handle_fail.
     rewrite bind_zero by typeclasses eauto.
-    eapply mleq_reflexive. }
+    reflexivity. }
   { rewrite free.free_bind_next.
     do 2 rewrite handle_next.
     rewrite bind_zero by typeclasses eauto.
-    eapply mleq_reflexive. }
+    reflexivity. }
   { rewrite free.free_bind_stop.
     do 2 rewrite handle_stop.
+    (* TODO equality must ignore [skip] *)
     (* TODO We are in trouble: we are trying to establish something
        that looks like the initial goal,
        where [m] has been instantiated with [eval η e]. *)
