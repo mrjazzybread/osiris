cd30dead8c6d443335ff5111ebfceb4de7a23f49
François Pottier <francois.pottier@inria.fr>
2023-03-02 18:25:01 +0100

    Prove [admits_iter].

--- theories/soundness.v
+++ theories/soundness.v
@@ -68,6 +68,40 @@ Qed.
 Definition admits {A} (m : div A) (s : spec A) :=
   ∀ φ, s ∋ φ → safe m φ.
 
+(* A step-indexed version of [admits] seems necessary in the proof of
+   the lemma [admits_iter]. *)
+
+Definition initially_admits {A} n (m : div A) (s : spec A) :=
+  ∀ φ, s ∋ φ → initially_safe n m φ.
+
+(* The four properties defined above form a diamond. *)
+
+Lemma unfold_admits {A} (m : div A) (s : spec A) :
+  admits m s ↔
+  ∀ n, initially_admits n m s.
+Proof.
+  unfold admits, initially_admits. firstorder.
+Qed.
+
+Lemma unfold_admits_one_way {A} (m : div A) (s : spec A) :
+  admits m s →
+  ∀ n, initially_admits n m s.
+Proof.
+  rewrite unfold_admits. eauto.
+Qed.
+
+(* [initially_admits] is monotonic in its step index. *)
+
+Lemma initially_admits_monotonic {A} (s : spec A) :
+  ∀ n1 n2 m,
+  initially_admits n2 m s →
+  n1 ≤ n2 →
+  initially_admits n1 m s.
+Proof.
+  unfold initially_admits.
+  eauto using initially_safe_monotonic.
+Qed.
+
 (* -------------------------------------------------------------------------- *)
 
 (* [ret a] and [ret a] are related. That is, the computation [ret a] admits
@@ -87,17 +121,21 @@ Qed.
    out of the judgement [admits m s] under a hypothesis about the observable
    behavior of the computation [m]. *)
 
+(* We state these lemmas in a strong form where they are able to exploit the
+   weak judgement [initially_admits], as opposed to the stronger judgement
+   [admits]. *)
+
 (* If [ret a] admits spec [s] then [ret a ≤ s]. That is, [ret a] is the best
    specification for [ret a]. *)
 
-Lemma invert_admits_RetF {A} (m : div A) (s : spec A) (a : A) φ :
-  admits m s →
+Lemma invert_admits_RetF {A} n (m : div A) (s : spec A) (a : A) φ :
+  initially_admits (S n) m s →
   observe m = RetF a →
   s ∋ φ →
   φ a.
 Proof.
-  unfold admits, safe. intros Hsafe Hm Hsφ.
-  specialize (Hsafe φ Hsφ 1).
+  unfold initially_admits. intros Hsafe Hm Hsφ.
+  specialize (Hsafe φ Hsφ).
   simpl in Hsafe.
   rewrite Hm in Hsafe.
   tauto.
@@ -107,15 +145,15 @@ Qed.
    admits the specification [s] and if [s] is nonempty, then a contradiction
    is obtained. *)
 
-Lemma invert_admits_Throw {A} (m : div A) (s : spec A) (k : void → div A) φ :
-  admits m s →
+Lemma invert_admits_Throw {A} n (m : div A) (s : spec A) (k : void → div A) φ :
+  initially_admits (S n) m s →
   observe m = VisF (Throw ()) k →
   s ∋ φ →
   False.
 Proof.
   (* The proof script is the same as that of the previous lemma! *)
-  unfold admits, safe. intros Hsafe Hm Hsφ.
-  specialize (Hsafe φ Hsφ 1).
+  unfold initially_admits. intros Hsafe Hm Hsφ.
+  specialize (Hsafe φ Hsφ).
   simpl in Hsafe.
   rewrite Hm in Hsafe.
   tauto.
@@ -123,14 +161,14 @@ Qed.
 
 (* The relation [admits m s] is stable under reduction of [m] to [m']. *)
 
-Lemma invert_admits_TauF {A} (m m' : div A) (s : spec A) :
-  admits m s →
+Lemma invert_admits_TauF {A} n (m m' : div A) (s : spec A) :
+  initially_admits (S n) m s →
   observe m = TauF m' →
-  admits m' s.
+  initially_admits n m' s.
 Proof.
-  (* The proof script is almost the same as in the previous two lemmas. *)
-  unfold admits, safe. intros Hsafe Hm φ Hsφ n.
-  specialize (Hsafe φ Hsφ (S n)).
+  (* The proof script is the same as that of the previous lemma! *)
+  unfold initially_admits. intros Hsafe Hm φ Hsφ.
+  specialize (Hsafe φ Hsφ).
   simpl in Hsafe.
   rewrite Hm in Hsafe.
   tauto.
@@ -142,16 +180,22 @@ Qed.
 
 (* This is a beautiful proof. *)
 
-Lemma admits_bind_aux {A B}
+(* We state this lemma in a strong form by requiring only the weak
+   hypothesis [∀ a, initially_admits n (m2 a) (s2 a)] instead of the stronger
+   hypothesis [∀ a, admits (m2 a) (s2 a)]. The weak form of this lemma would
+   suffice to prove [admits_bind]. However, the strong form of this
+   lemma is needed in the proof of [admits_iter]. *)
+
+Lemma initially_admits_bind {A B}
   (m2 : A → div B) (s1 : spec A) (s2 : A → spec B) φ :
-  (∀ a : A, admits (m2 a) (s2 a)) →
   bind s1 s2 ∋ φ →
   ∀ n (m1 : div A),
-  admits m1 s1 →
+  initially_admits n m1 s1 →
+  (∀ a : A, initially_admits n (m2 a) (s2 a)) →
   initially_safe n (bind m1 m2) φ.
 Proof.
-  intros Hms2 Hbφ.
-  induction n; intros m1 Hms1; [ simpl; tauto |].
+  intros Hbφ.
+  induction n; intros m1 Hms1 Hms2; [ simpl; tauto |].
   (* The goal is to prove that [bind m1 m2] is safe for [n+1] steps. *)
   rewrite unfold_bind.
   (* Proceed by cases on [m1]. Three cases arise. *)
@@ -160,19 +204,16 @@ Proof.
   (* Case: [m1] is [ret a]. *)
   { clear IHn. intros a Hm1.
     (* The goal is to prove that [m2 a] is safe for [n + 1] steps. *)
-    match goal with |- ?goal =>
-      change goal with (initially_safe (S n) (m2 a) φ)
-    end.
     (* From the fact that [ret a] admits the specification [s1],
        and from the fact that [s1] contains the postcondition
        [λ a, s2 a ∋ φ], we deduce that [s2 a ∋ φ] holds. *)
     unfold bind in Hbφ; simpl in Hbφ.
     assert (s2 a ∋ φ).
-    { apply (invert_admits_RetF m1 s1 a _ Hms1 Hm1 Hbφ). }
+    { apply (invert_admits_RetF _ m1 s1 a _ Hms1 Hm1 Hbφ). }
     clear Hms1 Hm1 Hbφ.
     specialize (Hms2 a).
-    (* The goal follows by definition of [admits]. *)
-    unfold admits, safe in Hms2. eauto. }
+    (* The goal follows by definition of [initially_admits]. *)
+    eauto. }
 
   (* Case: [m1] is [Skip m'1]. *)
   { intros m'1 Hm1.
@@ -181,7 +222,9 @@ Proof.
     (* This follows from the induction hypothesis and from the fact
        that [admits] is stable under reduction. *)
     eapply IHn.
-    eauto using invert_admits_TauF. }
+    { eauto using invert_admits_TauF. }
+    { eauto using initially_admits_monotonic with lia. }
+  }
 
   (* Case: [m1] fails. *)
   { clear IHn. intros X e k Hm1.
@@ -193,12 +236,79 @@ Proof.
 
 Qed.
 
-Lemma admits_bind {A B} :
-  ∀ (m1 : div A) (m2 : A → div B) (s1 : spec A) (s2 : A → spec B),
+Lemma admits_bind {A B}
+  (m1 : div A) (m2 : A → div B) (s1 : spec A) (s2 : A → spec B) :
   admits m1 s1 →
   (∀ a, admits (m2 a) (s2 a)) →
   admits (bind m1 m2) (bind s1 s2).
 Proof.
-  unfold admits at 3. unfold safe.
-  eauto using admits_bind_aux.
+  intros.
+  rewrite unfold_admits. unfold initially_admits.
+  eauto using initially_admits_bind, unfold_admits_one_way.
+Qed.
+
+(* -------------------------------------------------------------------------- *)
+
+(* [skip] preserves relatedness. *)
+
+Lemma initially_admits_skip {A} n (m : div A) (s : spec A) :
+  initially_admits n m s →
+  initially_admits (S n) (skip m) (skip s).
+Proof.
+  unfold initially_admits.
+  intros Hms.
+  unfold skip, monadskip_div, monadskip_spec, div_skip.
+  intros φ Hsφ. simpl. eauto.
+Qed.
+
+Lemma admits_skip {A} (m : div A) (s : spec A) :
+  admits m s →
+  admits (skip m) (skip s).
+Proof.
+  rewrite !unfold_admits.
+  intros. destruct n as [| n].
+  { unfold initially_admits. simpl. eauto. }
+  { eauto using initially_admits_skip. }
+Qed.
+
+(* -------------------------------------------------------------------------- *)
+
+(* [iter] preserves relatedness. *)
+
+Lemma initially_admits_iter {I A}
+  (body1 : I → div (I + A))
+  (body2 : I → spec (I + A)) :
+  (∀ i, admits (body1 i) (body2 i)) →
+  ∀ n i φ,
+  iter body2 i ∋ φ →
+  initially_safe n (iter body1 i) φ.
+Proof.
+  intros Hbody.
+  induction n; [ simpl; tauto |].
+  intros i φ Hφ.
+  erewrite unfold_iter by typeclasses eauto.
+  erewrite unfold_iter in Hφ by typeclasses eauto.
+  eapply initially_admits_bind.
+  { eexact Hφ. }
+  { eauto using unfold_admits_one_way. }
+  (* We must study two cases: continuing and finishing. *)
+  clear i φ Hφ. intros [ i | a ].
+  (* Case: the loop body wants to continue. *)
+  { eapply initially_admits_skip.
+    unfold initially_admits.
+    eapply IHn. }
+  (* Case: the loop body is done. *)
+  { eauto using unfold_admits_one_way, admits_ret. }
+Qed.
+
+Lemma admits_iter {I A}
+  (body1 : I → div (I + A))
+  (body2 : I → spec (I + A)) :
+  (∀ i, admits (body1 i) (body2 i)) →
+  ∀ i, admits (iter body1 i) (iter body2 i).
+Proof.
+  intros.
+  rewrite unfold_admits.
+  unfold initially_admits.
+  eauto using initially_admits_iter.
 Qed.
