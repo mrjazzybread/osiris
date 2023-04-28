49fea892f447e1f040d21385e0b1609e9211e287
Arnaud DABY-SEESARAM <ds-ac@nanein.fr>
2023-04-28 11:29:33 +0200

    Additional notations (printing only)

--- theories/notations.v
+++ theories/notations.v
@@ -1,5 +1,5 @@
 From iris Require Import base_logic.lib.gen_heap.
-Require Import free.
+Require Import free lang.
 
 (* ------------------------------------------------------------------------ *)
 
@@ -18,4 +18,18 @@ Notation "' x ← y ; z" :=
 (* Store-related notations *)
 
 Notation "l ↦ v" := (mapsto l (DfracOwn 1) v) (at level 20).
+
+
+(* ------------------------------------------------------------------------ *)
+
+(* Expression-related notations to make the expressions user-friendly. *)
+
+Notation "'λ' x '→' e" := (EFun x e) (at level 20, only printing).
+Notation "'let:' x 'in' e" := (ELet x e) (at level 20, only printing).
+Notation "bv ':b:' bvs" := (BiCons bv bvs) (at level 20, only printing).
+Notation "bv '.'" := (BiCons bv BiNil) (at level 20, only printing).
+Notation "patt '~>' v" := (Binding patt v) (at level 20, only printing).
+Notation "x" := (PVar x) (at level 19, only printing).
+Notation "x" := (EVar x) (at level 19, only printing).
+Notation "x '+I' y " := (EIntAdd x y) (at level 20, only printing).
 
