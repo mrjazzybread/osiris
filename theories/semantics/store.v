36be8ce214f1584390aa5802c025fac113eb6231
François Pottier <francois.pottier@inria.fr>
2023-05-06 14:13:16 +0200

    Some cleanup in store.v.

--- theories/semantics/store.v
+++ theories/semantics/store.v
@@ -2,38 +2,22 @@ From stdpp Require Import countable numbers gmap.
 From iris.prelude Require Import prelude options.
 From osiris.lang Require Import locations lang.
 
+(* This file defines the physical store. *)
 
-(* TODO:
-   - add the required lemmas so that the types declared below can remain opaque
-     to the rest of the development.
-   - declare the required instances of [ElemOf] and [Dom] so that the store can
-     be seen "as a gmap" (whatever its future definitions might be). *)
+(* The store is a map of locations to values. *)
 
+Definition store : Type := gmap loc val.
 
-(* -------------------------------------------------------------------------- *)
-(* The following section defines the physical store *)
+(* [store_ref] finds a fresh location to add to its argument.
+ * It returns the fresh location and the updated store. *)
+Definition store_ref (s: store) (v: val) : loc * store :=
+  let l := fresh_loc (dom s) in
+  (l, <[l := v]>s).
 
-Section Store.
-  Definition store : Type := gmap loc val.
-
-
-  (* [store_ref] finds a fresh location to add to its argument.
-   * It returns the fresh location and the updated store. *)
-  Definition store_ref (s: store) (v: val) : loc * store :=
-    let l := fresh_loc (dom s) in
-    (l, <[l := v]>s).
-
-  Lemma store_ref_dom (s: store) (v: val) :
-    let '(l, s') := store_ref s v in
-    dom s' = (dom s) ∪ {[ l ]} ∧ l ∉ dom s.
-  Proof.
-    split; first set_solver.
-    apply fresh_loc_fresh.
-  Qed.
-
-  Global Instance val_inhabited: Inhabited val := populate (VTuple VNil).
-
-End Store.
-
-(*Global Opaque fresh_locs.
-Global Opaque store.*)
+Lemma store_ref_dom (s: store) (v: val) :
+  let '(l, s') := store_ref s v in
+  dom s' = (dom s) ∪ {[ l ]} ∧ l ∉ dom s.
+Proof.
+  split; first set_solver.
+  apply fresh_loc_fresh.
+Qed.
