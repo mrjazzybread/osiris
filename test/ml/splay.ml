f8b1853b3f5284f6e988d39bc0c5708c83c7ebb3
François Pottier <francois.pottier@inria.fr>
2023-05-11 08:25:18 +0200

    Add a splay tree example.

---
+++ test/ml/splay.ml
@@ -0,0 +1,52 @@
+(* Code taken from "FP2: Fully in-Place Functional Programming"
+   by Anton Lorenzen, Daan Leijen, and Wouter Swierstra (2023). *)
+
+type 'a tree =
+  | Leaf
+  | Node of 'a tree * 'a * 'a tree
+
+type 'a zipper =
+  | Root
+  | NodeL of 'a zipper * 'a * 'a tree
+  | NodeR of 'a tree * 'a * 'a zipper
+
+let rec splay (l, x, r, ctx) =
+  match ctx with
+  | Root ->
+      Node (l, x, r)
+  | NodeL (Root, y, ry) ->
+      Node (l, x, Node (r, y, ry))
+  | NodeL (NodeR (lz, z, up), y, ry) ->
+      splay (Node (lz, z, l), x, Node(r, y, ry), up)
+  | NodeL (NodeL (up, z, rz), y, ry) ->
+      splay (l, x, Node (r, y, Node (ry, z, rz)), up)
+  | NodeR (ly, y, Root) ->
+      Node (Node (ly, y, l), x, r)
+  | NodeR (ly, y, NodeL (up, z, rz)) ->
+      splay (Node (ly, y, l), x, Node (r, z, rz), up)
+  | NodeR (ly, y, NodeR (lz, z, up)) ->
+      splay (Node(Node(lz, z, ly), y, l), x, r, up)
+
+and splay_leaf ctx =
+  match ctx with
+  | Root ->
+      Leaf
+  | NodeL (up, x, r) ->
+      splay (Leaf, x, r, up)
+  | NodeR(l, x, up) ->
+      splay (l, x, Leaf, up)
+
+let rec zlookup (t, x, ctx) : bool * 'a tree =
+  match t with
+  | Leaf ->
+      (false, splay_leaf ctx) (* not found, but splay anyway *)
+  | Node (l, y, r) ->
+      if x < y then
+        zlookup (l, x, NodeL (ctx, y, r)) (* go down left *)
+      else if x > y then
+        zlookup (r, x, NodeR (l, y, ctx)) (* go down right *)
+      else
+        (true, splay (l, y, r, ctx))
+
+let lookup t x =
+  zlookup (t, x, Root)
