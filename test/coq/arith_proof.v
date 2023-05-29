93bcff88b1038c26efb9d5673b7831e30ebd7a8d
Arnaud DABY-SEESARAM <ds-ac@nanein.fr>
2023-05-29 22:01:38 +0200

    Specification of mutually recursive functions

--- test/coq/arith_proof.v
+++ test/coq/arith_proof.v
@@ -9,24 +9,44 @@ From osiris Require Import osiris.
 From osiris.libs Require Import Stdlib.
 From test Require Import arith.
 
+(* -------------------------------------------------------------------------- *)
+
+(* The current file is a sandbox to develop tactics on the specification of
+   mutually recursive functions. *)
+
+(* -------------------------------------------------------------------------- *)
+
 Context `{!osirisGS_gen hlc Σ}.
 
+(* -------------------------------------------------------------------------- *)
+
+(* Definition of the specifications. They have to be persistent for the tactics
+   to work properly. *)
+
 Definition add_spec vadd : iProp Σ :=
-  ∀ (x : Z),
+  □ (∀ (x : Z),
     ⌜0 <= x⌝%Z →
     WP call vadd #x
     {{ λ v,
         ∀ (y: Z),
           ⌜0 <= y⌝%Z →
-        WP call v #y {{ λ res, ⌜res = #(x + y)%Z⌝ }} }}.
+        WP call v #y {{ λ res, ⌜res = #(x + y)%Z⌝ }} }}).
 
 Definition mult_spec vmult : iProp Σ :=
-  ∀ (x: Z),
+  □ (∀ (x: Z),
     ⌜0 <= x⌝%Z →
     WP call vmult #x
        {{ λ v, ∀ (y: Z),
              ⌜0 <= y⌝%Z →
-             WP call v #y {{ λ res, ⌜res = #(x * y)%Z⌝ }} }}.
+             WP call v #y {{ λ res, ⌜res = #(x * y)%Z⌝ }} }}).
+
+Definition trivial_spec: val → iProp Σ := λ v, ⌜ True ⌝%I.
+Definition is_equal e : val → iProp Σ := λ v, ⌜v = #e⌝%I.
+
+(* -------------------------------------------------------------------------- *)
+
+(* As there is no proper notation for specifications of closures yet, I added
+   the following for the goal to remain readable. *)
 
 Local Notation "'Spec'  'of'  'the'  'addition.'" :=
   (add_spec _)
@@ -36,16 +56,340 @@ Local Notation "'Spec'  'of'  'the'  'multiplication.'" :=
   (mult_spec _)
   (only printing).
 
-Definition trivial_spec: val → iProp Σ := λ v, ⌜ True ⌝%I.
-Definition is_equal e : val → iProp Σ := λ v, ⌜v = #e⌝%I.
+(* -------------------------------------------------------------------------- *)
 
-(* Integer representations do not matter for now. *)
+(* Whether or not integers are representable does not matter for now. *)
 Axiom int_representable:
   forall (i: Z), (int.min_signed ≤ i ≤ int.max_signed)%Z.
 
+(* -------------------------------------------------------------------------- *)
 
-(* The specification of [Add] will be proven several times. *)
+(* [Ltac] helpers to deal with value abstraction. *)
 
+Tactic Notation "oAbstract" constr(s1) ident(i1):=
+  lazymatch goal with
+  | |- context [VCloRec ?η ?bds s1] =>
+      generalize (VCloRec η bds s1); intro i1
+  end.
+Tactic Notation "oAbstract"
+       constr(s1) ident(i1)
+       constr(s2) ident(i2) :=
+  oAbstract s1 i1;
+  oAbstract s2 i2.
+
+(* [oCall] behaves in the same way that wp_call does, except that it abstracts
+   the required closures.
+   It is defined as a notation so that it is easy to ask for more idents (Ltac
+   cannot take a list of idents as argument for a tactic). *)
+Tactic Notation "oCall" constr(s1) ident(i1):=
+  with_strategy transparent [call] unfold call; simpl (bind _ _);
+  lazymatch goal with
+  | |- context [VCloRec ?η ?bds s1] =>
+      generalize (VCloRec η bds s1); intro i1
+  end;
+  wp.
+Tactic Notation "oCall" constr(s1) ident(i1) constr(s2) ident(i2) :=
+  with_strategy transparent [call] unfold call; wp;
+  lazymatch goal with
+  | |- context [VCloRec ?η ?bds s1] =>
+      generalize (VCloRec η bds s1); intro i1
+  end;
+  lazymatch goal with
+  | |- context [VCloRec ?η ?bds s2] =>
+      generalize (VCloRec η bds s2); intro i2
+  end.
+
+(* [oSpecify] is used to provide the user the possibility to provide
+   specifications for variables which are about to be added to the environment
+   through [dconcatenating].
+   The tactic notation only works for two arguments, as it is what is required
+   here. If it works, it should be moved to [proofmode/specifications.v].
+   Note: [dconcatenating] only takes two arguments now (the continuation is
+         always ret). *)
+Tactic Notation "oSpecify"
+       constr(n1) constr(spec1) constr(H1) ident(i1)
+       constr(n2) constr(spec2) constr(H2) ident(i2) :=
+  lazymatch goal with
+  | |- environments.envs_entails
+         _ (wp _ _ (dconcatenating ?δ _) _) =>
+      let v1 := eval cbn in (δ !!! n1) in
+      let v2 := eval cbn in (δ !!! n2) in
+      let hyps := eval cbn in (foldr String.append "" [H1; H2]) in
+      iApply
+        (prove_specify [
+              spec1 v1;
+              spec2 v2
+        ]);
+      [ by eauto using intuitionistically_persistent
+      | (* A Specification is persistent. *) iModIntro;
+          (* Upon proving the addition, one can have access to Löb-like IH. *)
+          iIntros hyps
+      | iModIntro; iIntros hyps
+      | iIntros H1;
+        iIntros H2;
+        oAbstract n1 vadd
+                  n2 vmult;
+        wp_continue]
+  end.
+
+
+(* The specification of [Add] will be proven several times. Each attempt should
+   improve the proof --- especially the way to handle mutually recursive
+   functions. *)
+
+(* -------------------------------------------------------------------------- *)
+
+(* Sixth version: It should no longer be necessary to provide en environment [η]
+                  which contains the standard library as a module. *)
+
+(* -------------------------------------------------------------------------- *)
+
+(* Fifth version: Once the specifications proven, the [wp] tactic should know
+                  how to handle them (ie. use their spec as soon as possible,
+                  and handle their partial applications automatically!). *)
+
+(* -------------------------------------------------------------------------- *)
+
+(* Fourth version: The process of declaring specifications should be
+                   interactive. *)
+
+(* -------------------------------------------------------------------------- *)
+
+(* Third version: a lemma exists to better reason on mutually recursive
+                  functions. *)
+
+Lemma Add_spec3 :
+    let Λ :=
+    [
+      ("add", add_spec);
+      ("mult", mult_spec);
+      ("i3", is_equal #3)
+    ]
+  in
+  let η := EnvCons "Stdlib" Stdlib $
+           EnvNil in
+  ⊢ WP eval_mexpr η Arith {{ module_spec Λ }}.
+Proof.
+  intros.
+  wp.
+
+  oSpecify "add" add_spec "#Hadd" vadd
+           "mult" mult_spec "#Hmult" vmult.
+
+  (* Specification of the addition. *)
+  { (* The rest of the proof is unchanged. *) admit. }
+
+  (* Specification of the multiplication. *)
+  { (* The rest of the proof is unchanged. *) admit. }
+
+  (* The rest of the proof is unchanged. *)
+Admitted.
+
+(* -------------------------------------------------------------------------- *)
+
+(* Second version: the values of [add] and [mult] are opacified manually at "the
+                   right time". *)
+
+Lemma Add_spec2 :
+    let Λ :=
+    [
+      ("add", add_spec);
+      ("mult", mult_spec);
+      ("i3", is_equal #3)
+    ]
+  in
+  let η := EnvCons "Stdlib" Stdlib $
+           EnvNil in
+  ⊢ WP eval_mexpr η Arith {{ module_spec Λ }}.
+Proof.
+  intros.
+  wp.
+
+  (* [add] and [mult] are recursively defined.
+     Intuitively, one wants:
+     1. to provide the required specs for the environment;
+     2. to prove each spec one by one using Löb Induction.
+
+     Although it is not the case here, it might also be necessary to provide
+     external hypotheses to prove the specs.
+
+     The following does *not* work as expected. As the [RecBinding]s take
+     functions as argument and we only generalize values in the current process,
+     the specs hidden behind the [▷] are unusable (and useless).
+     Therefore, one should abstract the bodies of the function, and prove a spec
+     for them. *)
+
+  lazymatch goal with
+  | |- environments.envs_entails
+         ?Δ (wp ?s ?E
+                (dconcatenating
+                   (EnvCons "add" ?vadd $
+                            EnvCons "mult" ?vmult $
+                            EnvNil)
+                   ?δη)
+                ?φ) =>
+      iAssert (add_spec vadd ∗ mult_spec vmult ∗ emp)%I as "(#Hadd & #Hmult & _)"
+  end;
+  [ (* In order to prove the assertion, we use Löb induction. *)
+    iLöb as "IH";
+    iDestruct "IH" as "(#Hadd&#Hmult&_)";
+    iSplitL "" ; (* Proving the addition does not require prior resources. *)
+    [
+    | iSplitL "";
+      [
+      | done ]
+    ]
+  | (* After the specifications have been proven, abstract the addition and
+       multiplication and .continue. *)
+    oAbstract "add" vadd; oAbstract "mult" vmult; wp_continue ].
+
+  { (* ---------------------------------------------------------------------- *)
+    (* Proof of [add]. *)
+    { iModIntro. iIntros(i1 H1).
+
+      (* It is during the function call to add that the environment is extended
+         to include the definition of [mult].*)
+      oCall "add" vadd "mult" vmult. wp. wp_continue.
+
+      (* The proof can now continue as expected. *)
+      iIntros(i2 H2). wp. wp_continue.
+      wp_use Stdlib__eq__spec; try done; try apply int_representable.
+      iIntros (veq_part) "Hspec_eq".
+      wp.
+      iApply (wp_covariant with "Hspec_eq"). (* TODO: fix [wp_use]. *)
+      iIntros (? ->).
+      destruct (i2 =? 0)%Z eqn:E; wp.
+      { (* [i2 = 0%Z] *)
+        apply Z.eqb_eq in E as ->.
+        wp_use "Hmult"; first done.
+        iIntros (Hmult_part) "Hmult_part".
+        iApply (wp_covariant with "[Hmult_part]").
+        { iApply "Hmult_part"; done. }
+        iIntros(?->).
+        iPureIntro. do 2 f_equal. lia. }
+      { (* [i2 != 0%Z], goal: [1 + (add x (y - 1)) == x + y] *)
+        wp_par.
+        { (* [λ y, 1 + y] *) iIntros (vpartial) "H". iExact "H". }
+        { (* [add x (y - 1)]. *)
+          wp_par.
+          { wp_use "Hadd"; iPureIntro; exact H1. }
+          - wp_call.
+            instantiate (1 := λ v, ⌜ v = # (i2 - 1)%Z ⌝%I).
+            by rewrite int.sub_repr_repr.
+          - iIntros (vpadd ?) "Hadd_partial ->".
+            wp_use "Hadd_partial".
+            iPureIntro. lia. }
+        { iIntros (vadd1 ?) "Hadd1 ->".
+          wp.
+          iSpecialize ("Hadd1" $! (i1 + (i2 - 1))%Z NotStuck top). wp.
+          iApply (wp_covariant with "Hadd1").
+          iIntros (?->).
+          iPureIntro. do 2 f_equal. lia. } } } }
+
+    (* ------------------------------------------------------------------ *)
+    (* Proof of [mult]. *)
+    { iModIntro. iIntros (i1 H1).
+
+      (* It is during the function call to add that the environment is extended
+         to include the definition of [mult].*)
+      oCall "add" vadd "mult" vmult. wp_continue.
+
+      (* The proof can now continue as expected. *)
+      iIntros(i2 H2). wp. wp_continue.
+      wp_use Stdlib__eq__spec; try done; try apply int_representable.
+      iIntros (veq_part) "Heq_part".
+      wp. iApply (wp_covariant with "Heq_part").
+      iIntros (?->).
+      destruct (i2 =? 0)%Z eqn:E.
+      { (* [i2 = 0%Z]. *) wp.
+        apply Z.eqb_eq in E as ->. iPureIntro. do 2 f_equal. lia. }
+      { (* [i2 != 0%Z]. *) wp.
+        wp_use Stdlib__eq__spec; try done; try apply int_representable.
+        clear veq_part.
+        iIntros (veq_part) "Heq_part".
+        wp. iApply (wp_covariant with "Heq_part").
+        iIntros (?->).
+        destruct (i2 =? 1)%Z eqn:E1.
+        { (* [i2 = 1%Z]. *) wp.
+          apply Z.eqb_eq in E1 as ->. iPureIntro. do 2 f_equal. lia. }
+        { (* [i2 <> 1%Z]. *) wp.
+          (* Proof that [add x (mult x (y - 1)) == x * y]. *)
+          wp_par.
+          { (* [λ y, add x y]. *)
+            iSpecialize ("Hadd" $! i1 H1).
+            wp_use "Hadd". }
+          { (* [mult x (y - 1)] *)
+            wp_par.
+            { (* [λ y, mult x y]. *)
+              wp_use "Hmult"; iPureIntro; exact H1. }
+            { (* [y - 1]. *) wp_call.
+              instantiate (1 := λ v, ⌜ v = # (i2 - 1)%Z ⌝%I).
+              by rewrite int.sub_repr_repr. }
+            { iIntros (vmult_part ?) "Hmult_part ->".
+              wp. wp_use "Hmult_part".
+              iPureIntro. lia. } }
+          { (* Finish the application of [add]. *)
+            iIntros (vadd_part ?) "Hadd_part ->".
+            wp.
+            unshelve iSpecialize ("Hadd_part" $! (i1 * (i2 - 1))%Z _).
+            { apply Ztac.mul_le; lia. }
+            iApply (wp_covariant with "Hadd_part").
+            iIntros (?->).
+            iPureIntro. do 2 f_equal.
+            lia. } } } }
+
+  wp_par;
+    [ wp_use "Hadd"; done
+    | wp_use "Hadd";
+      [ done | iIntros (vadd_part) "Hadd_part"; wp; by iApply "Hadd_part" ]
+    | ].
+  iIntros (vadd_part ?) "Hadd_part ->". wp.
+  iSpecialize("Hadd_part" $! _ _).
+  iApply (wp_covariant with "Hadd_part").
+  Unshelve. 2: lia.
+  iIntros (?->). wp.
+  wp_specify "i3" (is_equal #3); first done.
+  iIntros (i3) "#Hi3". wp_continue.
+
+  repeat wp_par.
+  { wp_use "Hmult"; first done.
+    iIntros (vmult_part) "Hmult_part".
+    iSpecialize ("Hmult_part" $! _ _). wp.
+    iApply (wp_covariant with "Hmult_part").
+    iIntros (?->).
+    wp_use "Hadd"; first done. }
+  { wp_use "Hadd"; first done. }
+  { wp_use "Hmult"; first done. }
+  { wp_use "Hadd"; first done.
+    iIntros (vadd_part') "Hadd_part'".
+    iSpecialize ("Hadd_part'" $! _ _).
+    iApply (wp_covariant with "Hadd_part'").
+    iIntros(?->). by wp_set_postcondition. }
+  { iIntros (v1 ?) "H1 ->". wp.
+    iSpecialize ("H1" $! _ _).
+    iApply (wp_covariant with "H1").
+    iIntros (?->). by wp_set_postcondition. }
+  { iIntros (v1 ?) "H1 ->". wp.
+    iSpecialize ("H1" $! _ _).
+    iApply (wp_covariant with "H1").
+    iIntros (?->). by wp_set_postcondition. }
+  { iIntros (v1 ?) "H1 ->". wp.
+    iSpecialize ("H1" $! _ _).
+    iApply (wp_covariant with "H1").
+    iIntros (?->). wp. wp_continue.
+
+    wp_module_spec. }
+
+  Unshelve.
+  all: done.
+Time Qed.
+
+(* -------------------------------------------------------------------------- *)
+
+(* Initial version: the values of [add] and [mult] are abstracted away after
+                    having proven their specifications. Unfortunately, [add]
+                    (resp. [mult]) is not opaque when proving [mult]
+                    (resp. [add]). *)
 Lemma Add_spec :
     let Λ :=
     [
@@ -97,9 +441,8 @@ Proof.
         | |- context [VCloRec η ?rbds "mult"] =>
             generalize (VCloRec η rbds "mult")
         end *) .
-      iIntros (* vmult *) "(#Hadd&#Hmult&_)".
-      iIntros(i1 H1). wp_call. wp_continue.
-      iIntros(i2 H2). wp. wp_continue.
+      iIntros (* vmult *) "(#Hadd&#Hmult&_) !>".
+      iIntros(i1 H1). wp_call. wp_continue. iIntros(i2 H2). wp. wp_continue.
       wp_use Stdlib__eq__spec; try done; try apply int_representable.
       iIntros (veq_part) "Hspec_eq".
       wp.
@@ -141,7 +484,7 @@ Proof.
         | |- context [VCloRec η ?rbds "add"] =>
              generalize (VCloRec η rbds "add")
         end *) .
-      iIntros (* vadd *) "(#Hadd&#Hmult&_)".
+      iIntros (* vadd *) "(#Hadd&#Hmult&_) !>".
       iIntros (i1 H1). wp_call. wp_continue.
       iIntros(i2 H2). wp. wp_continue.
       wp_use Stdlib__eq__spec; try done; try apply int_representable.
@@ -186,14 +529,8 @@ Proof.
             iPureIntro. do 2 f_equal.
             lia. } } } }
     done. }
-  lazymatch goal with
-  | |- context [VCloRec η ?rbds "add"] =>
-      generalize (VCloRec η rbds "add")
-  end; intros vadd.
-  lazymatch goal with
-  | |- context [VCloRec η ?rbds "mult"] =>
-      generalize (VCloRec η rbds "mult")
-  end; intros vmult.
+  oAbstract "add" vadd
+            "mult" vmult.
   iDestruct "H" as "(#Hadd & #Hmult & _)".
   wp_continue.
 
--- theories/weakestpre/specifications.v
+++ theories/weakestpre/specifications.v
@@ -9,11 +9,13 @@ From osiris.lang Require Import lang.
 From osiris.semantics Require Import semantics.
 From osiris.weakestpre Require Import wp wp_tactics.
 
+(* This file should move to the [proofmode] directory. *)
+
 
 
 (* The follwoing section defines the typeclasses to use to define a
    specification. *)
-Section TypeclassesDefinitions.
+Section FunctionSpecifications.
   Context `{!osirisGS_gen hlc Σ}.
 
   (* [pure_unary_spec] is used to tell osiris that [f] represents a closure
@@ -55,4 +57,180 @@ Section TypeclassesDefinitions.
                        φ vpartial) -∗ (* TODO: get a later here. *)
         wp s E (call f #a) φ.
 
-End TypeclassesDefinitions.
+End FunctionSpecifications.
+
+(* -------------------------------------------------------------------------- *)
+
+(* The following section provides helper lemmas to reason on mutually-recursive
+   functions ([let rec ... and ...] and [let ... = ...] constructs). *)
+Section MutuallyRecursive.
+
+  Context `{!osirisGS_gen hlc Σ}.
+
+  (* ------------------------------------------------------------------------ *)
+
+  (* The idea of this section is to allow to prove goals of the form
+     [wp s E (concatenating ...) φ] and [wp s E (dconcatenating ...) φ] by
+     proving that it is enough to prove:
+     [ ▷ spec1 v1 -∗ ... -∗ ▷ specn vn -∗ spec1 v1 ],
+      ... ,
+     [ ▷ spec1 v1 -∗ ... -∗ ▷ specn vn -∗ specn vn ], and
+     [ (spec1 v1 -∗ ... -∗ specn vn -∗ P) ],
+     where [P] represents the initial goal to prove.
+
+     Note: it does not seem possible to prove the lines above in separate
+           branches of the proof (ie. outside of the Iris proofmode) as one may
+           need resources to prove the specifications.
+           There does not seem to be an easy way to share the resources and keep
+           the built-in Löb induction. *)
+
+  (* ------------------------------------------------------------------------ *)
+
+  (* Definitions to build the lemmas on mutually-recursive functions. *)
+
+  Definition conjn (pl: list (iProp Σ)) : iProp Σ :=
+    foldr (fun e res => e ∗ res)%I emp%I pl.
+
+  Definition wandn (pl: list (iProp Σ)) (P: iProp Σ) : iProp Σ :=
+    foldr (fun e res => e -∗ res)%I P pl.
+
+  Definition sepn_wand (pl: list (iProp Σ)) (P: iProp Σ) : iProp Σ :=
+    conjn pl -∗ P.
+
+  Lemma wandn_sepn_wand_1 (pl: list (iProp Σ)) (P: iProp Σ) :
+    wandn pl P -∗ sepn_wand pl P.
+  Proof.
+    iInduction (pl) as [ | p pl ] "IH";
+      unfold wandn; unfold sepn_wand; simpl; auto.
+
+    iIntros "H_impl [Hp H_conj]".
+    iApply ("IH" with "[H_impl Hp] H_conj").
+    iApply ("H_impl" with "Hp").
+  Qed.
+
+  Lemma wandn_sepn_wand_2 (pl: list (iProp Σ)) (P: iProp Σ) :
+    sepn_wand pl P -∗ wandn pl P.
+  Proof.
+    iInduction (pl) as [ | p pl ] "IH";
+      unfold wandn; unfold sepn_wand; simpl.
+
+    { iIntros "H". iApply ("H" with "[//]"). }
+
+    iIntros "H_sepn Hp".
+    iPoseProof (wand_curry with "H_sepn Hp") as "H_sepn".
+    iApply ("IH" with "H_sepn").
+  Qed.
+
+  (* ------------------------------------------------------------------------ *)
+
+  Lemma conjn_later_1 (pl: list (iProp Σ)) :
+    ▷ conjn pl -∗ conjn (map (fun e => ▷ e)%I pl).
+  Proof.
+    iInduction pl as [ | p pl ] "IH"; simpl; auto.
+    iIntros "[$ Hpl]".
+    iApply ("IH" with "Hpl").
+  Qed.
+
+  Lemma conjn_later_2 (pl: list (iProp Σ)) :
+    conjn (map (fun e => ▷ e)%I pl) -∗ ▷ conjn pl.
+  Proof.
+    iInduction pl as [ | p pl ] "IH"; simpl; auto.
+    iIntros "[$ Hpl]".
+    iApply ("IH" with "Hpl").
+  Qed.
+
+  Lemma conjn_pers (pl: list (iProp Σ)) :
+    Forall Persistent pl →
+    Persistent (conjn pl).
+  Proof.
+    induction pl as [ | p pl IH].
+    { simpl. intros. apply emp_persistent. }
+
+    { intros [Hp Hpl]%Forall_cons.
+      simpl. apply sep_persistent; first exact Hp.
+      exact (IH Hpl). }
+  Qed.
+
+  Lemma conjn_löb pl :
+    (□ (conjn (map (fun e => ▷ e)%I pl) -∗ conjn pl)) -∗
+    conjn pl.
+  Proof.
+    iIntros "#H1".
+    iLöb as "Hl".
+    iApply "H1".
+    iApply (conjn_later_1 with "Hl").
+  Qed.
+
+  Lemma wand_conjn_l (pl: list (iProp Σ)) (P: iProp Σ) :
+    Persistent P →
+    conjn (map (fun p => P -∗ p) pl)%I -∗
+    (P -∗ conjn pl).
+  Proof.
+    intros Hpers.
+    iInduction (pl) as [ | p pl ] "IH"; auto; simpl.
+    iIntros "[Hp Hpl] #HP".
+    iPoseProof ("IH" with "Hpl HP") as "$".
+    iApply ("Hp" with "HP").
+  Qed.
+
+  Lemma persistent_intro (P: iProp Σ):
+    (Persistent P -> ⊢ P -∗ □ P)%I.
+  Proof. auto. Qed.
+
+  Lemma conjn_persistent_l (pl: list (iProp Σ)) :
+    conjn (map (λ e, □ e)%I pl) -∗ (□ conjn pl).
+  Proof.
+    iInduction (pl) as [ | p pl ] "IH"; auto; simpl.
+    assert (Persistent (conjn (map (λ e, □ e)%I pl))).
+    { apply conjn_pers. induction pl as [ | ? ? IHpl ]; simpl;
+        [ by apply Forall_nil | apply Forall_cons; split].
+      - apply intuitionistically_persistent.
+      - apply IHpl. }
+    iIntros "[#Hp #Hpl]".
+    iSplitL; first by auto.
+    iApply ("IH" with "Hpl").
+  Qed.
+
+  Lemma conjn_weaken2 (f g: iProp Σ → iProp Σ) (pl: list (iProp Σ)) :
+    (forall e, f e -∗ g e) → conjn (map f pl) -∗ conjn (map g pl).
+  Proof.
+    iIntros (Hf).
+    induction pl as [ | p pl IH ]; simpl; auto.
+    iIntros "[Hp Hpl]".
+    iSplitL "Hp".
+    { iApply (Hf with "Hp"). }
+    { iApply (IH with "Hpl"). }
+  Qed.
+
+  Lemma prove_specify (pl: list (iProp Σ)) (P: iProp Σ) :
+    Forall Persistent pl → ⊢
+    (*□ (wandn (map (fun e => ▷ e)%I pl) (conjn pl)) -∗*)
+    (wandn
+       (map (λ p, □ p)
+       (map (λ p, wandn (map (λ e : iPropI Σ, ▷ e) pl) p) pl)))%I
+    ((wandn pl P) -∗
+    P).
+  Proof.
+    intros Hpers.
+    iApply wandn_sepn_wand_2.
+    iIntros "Hspecs1 HP"; unfold sepn_wand.
+    iPoseProof (conjn_persistent_l with "Hspecs1") as "#Hspecs3".
+    iPoseProof (wandn_sepn_wand_1 with "HP") as "HP".
+    unfold sepn_wand.
+
+    assert (Persistent (conjn (map (λ e, ▷ e)%I pl))).
+    { apply conjn_pers.
+      induction pl as [ | p pl IH]; first by apply Forall_nil.
+      apply Forall_cons in Hpers as [Hp Hpl].
+      apply Forall_cons; split;
+        [ exact (later_persistent _ Hp)
+        | exact (IH Hpl) ]. }
+    iPoseProof (conjn_weaken2 with "Hspecs3") as "#Hspecs".
+    { iIntros (e) "H".
+      iApply (wandn_sepn_wand_1 with "H"). }
+    iPoseProof ((wand_conjn_l pl (conjn (map (λ e : iPropI Σ, ▷ e)%I pl))
+                              H) with "Hspecs") as "Hspecs2".
+    iPoseProof (conjn_löb with "Hspecs2") as "Hspecs4".
+    iApply ("HP" with "Hspecs4").
+  Qed.
+End MutuallyRecursive.
