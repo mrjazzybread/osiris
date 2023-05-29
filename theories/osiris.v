a9fc011dd1df8f7e28bd1c510aecfaeb2fafc759
François Pottier <francois.pottier@inria.fr>
2023-05-29 14:28:47 +0200

    Comment.

--- theories/osiris.v
+++ theories/osiris.v
@@ -1,4 +1,9 @@
 From osiris Require Export base.
 From osiris.lang Require Export lang.
 From osiris.semantics Require Export semantics.
+  (* TODO semantics.v should load evalprime *)
 From osiris.weakestpre Require Export weakestpre.
+(* From osiris.proofmode Require Export proofmode. *)
+  (* TODO should do this, but this breaks some proofs
+          because it makes [eval] opaque
+          and introduces [eval']. *)
