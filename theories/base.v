3e3a66364e56abe07e9279b1ccf91e78cd8bb37a
François Pottier <francois.pottier@inria.fr>
2023-06-13 16:02:06 +0200

    base.v: add a couple of logical lemmas.

--- theories/base.v
+++ theories/base.v
@@ -1,2 +1,18 @@
 Require Export Coq.Program.Equality.
 From stdpp Require Export base strings.
+
+(* Logical tautologies. *)
+
+Lemma share_common_conjunct (P Q Q' : Prop) :
+  (P →      Q ↔ Q') →
+  P ∧ Q  ↔  P ∧ Q'.
+Proof.
+  tauto.
+Qed.
+
+Lemma share_common_hypothesis {X} (Q Q' : X → Prop) :
+  (∀ x,          Q x ↔ Q' x) →
+  (∀ x, Q x)  ↔  (∀ x, Q' x).
+Proof.
+  firstorder.
+Qed.
