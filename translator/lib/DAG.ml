3b7c0bb83d65ee9b8b150726cdf66eebc7bf40a4
Arnaud DABY-SEESARAM <ds-ac@nanein.fr>
2023-06-20 08:51:58 +0200

    [translator] New interface for DAG.

--- translator/lib/DAG.ml
+++ translator/lib/DAG.ml
@@ -1,25 +1,34 @@
 type 'a t =
-  | Root of 'a
+  | Leaf of 'a
   | Node of 'a * 'a t list
 
-let init (a: 'a) : 'a t = Root a
+let init (a: 'a) : 'a t = Leaf a
 
-let add (g: 'a t) (l: 'a list) : 'a t =
+let rec map (f: 'a -> 'b) : 'a t -> 'b t = function
+  | Leaf a -> Leaf (f a)
+  | Node (a, l) ->
+     Node (f a, List.map (map f) l)
+
+let rec flat_map (f: 'a -> 'b t) (g: 'a t) : 'b t =
   match g with
-  | Root a -> Node (a, List.map init l)
-  | Node (n, l') -> Node (n, List.map init l @ l')
+  | Leaf a -> f a
+  | Node (a, l) ->
+     let l: 'b t list = List.map (flat_map f) l in
+     match f a with
+     | Leaf a -> Node (a, l)
+     | Node (a, l') -> Node (a, l' @ l)
 
 let rec size : 'a t -> int = function
-  | Root _ -> 1
+  | Leaf _ -> 1
   | Node (_, l) -> 1 + List.fold_right (fun g i -> i + size g) l 0
 
 let rec to_list = function
-  | Root a -> [a]
+  | Leaf a -> [a]
   | Node (a, []) -> [a]
   | Node (a, h :: t) ->
      to_list h @ to_list (Node (a, t))
 
-let rec map (f: 'a -> 'b) : 'a t -> 'b t = function
-  | Root a -> Root (f a)
-  | Node (a, l) ->
-     Node (f a, List.map (map f) l)
+let add l a =
+  match a with
+  | Leaf a -> Node (a, l)
+  | Node (a, l') -> Node (a, l' @ l)
--- translator/lib/DAG.mli
+++ translator/lib/DAG.mli
@@ -2,10 +2,12 @@ type 'a t
 
 val init : 'a -> 'a t
 
-val add : 'a t -> 'a list -> 'a t
-
 val size : 'a t -> int
 
 val to_list : 'a t -> 'a list
 
+val add : 'a t list -> 'a t -> 'a t
+
 val map : ('a -> 'b) -> 'a t -> 'b t
+
+val flat_map : ('a -> 'b t) -> 'a t -> 'b t
