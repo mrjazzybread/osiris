5035c1ccf2197d69861d022261ec4685f36845b5
François Pottier <francois.pottier@inria.fr>
2023-06-22 17:57:19 +0200

    specifications.v: remove [pure_unary_spec] and [pure_binary_spec].
    Pure functions should be specified using [simp], not [WP].

--- theories/weakestpre/specifications.v
+++ theories/weakestpre/specifications.v
@@ -11,55 +11,6 @@ From osiris.weakestpre Require Import wp wp_tactics.
 
 (* This file should move to the [proofmode] directory. *)
 
-
-
-(* The follwoing section defines the typeclasses to use to define a
-   specification. *)
-Section FunctionSpecifications.
-  Context `{!osirisGS_gen hlc Σ}.
-
-  (* [pure_unary_spec] is used to tell osiris that [f] represents a closure
-     whose behaviour is that of [ff], a unary Gallina function. *)
-  (* TODO for pure functions, use [v = #a → simp (call f v) (ret #(ff a))]. *)
-  Class pure_unary_spec
-        {A B: Type} `{!Encode A} `{!Encode B}
-        (f: val) (ff: A → B) :=
-    spec:
-      ∀ (b: A) s E (φ: val → iProp Σ),
-        ▷ (φ #(ff b)) -∗
-        wp s E (call f #b) φ.
-
-  (* [pure_binary_spec] is used to tell osiris that [f] represents a closure
-     whose behaviour is that of [ff], a binary Gallina function. *)
-  Class pure_binary_spec
-        {A B C: Type} `{!Encode A} `{!Encode B} `{!Encode C}
-        (f: val) (ff: A → B → C) (a: A) (b: B) :=
-    spec2:
-      ∀ s E (φ: val → iProp Σ),
-        ▷ (φ #(ff a b)) -∗
-        wp s E (call f #a) (λ (v: val), wp s E (call v #b) φ).
-  Global Hint Mode pure_binary_spec - - - - - - - - ! !
-    : typeclass_instances.
-
-
-  (* [pure_partial_binary_spec] is used to tell osiris that [f] represents a
-     closure whose behaviour is that of [ff], a binary Gallina function.
-     Using this typeclass will allow for automation when encounting partial
-     applications of [f]. *)
-  Class pure_binary_partial_spec
-        {A B C: Type} `{!Encode A} `{!Encode B} `{!Encode C}
-        (f: val) (ff: A → B → C) :=
-    spec2_1:
-      ∀ (a: A) s E (φ: val → iProp Σ),
-         (∀ (vpartial: val),
-                       (∀ (b: B) s' E',
-                           wp s' E' (call vpartial #b)
-                              (λ res, ⌜res = #(ff a b)⌝)) -∗
-                       φ vpartial) -∗ (* TODO: get a later here. *)
-        wp s E (call f #a) φ.
-
-End FunctionSpecifications.
-
 (* -------------------------------------------------------------------------- *)
 
 (* The following section provides helper lemmas to reason on mutually-recursive
