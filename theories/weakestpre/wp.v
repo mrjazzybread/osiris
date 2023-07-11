757c19d40196fad51852e0d02783a55d658d2d33
Arnaud DABY-SEESARAM <ds-ac@nanein.fr>
2023-07-11 09:24:29 +0200

    [adequacy] corollary of [wp_adequacy].

--- theories/weakestpre/wp.v
+++ theories/weakestpre/wp.v
@@ -490,22 +490,22 @@ Section Adequacy.
 
   (* Example of a proposition [φ]:
        match is_ret m2 with
-       | Some v => φ' v ={top, ∅}=∗ ⌜φ⌝
+       | Some v => φ' v ={⊤, ∅}=∗ ⌜φ⌝
        | None =>  ⌜ ~ stuck (σ2, m2) ⌝
        end)) → *)
-  Lemma wp_strong_adequacy_gen Σ `{!invGpreS Σ}
+  Lemma wp_adequacy Σ `{!invGpreS Σ}
         `{Hstore: @gen_heapGS loc val Σ loc_eq_decision loc_countable}
         s m1 σ1 n σ2 m2 (φ: Prop) :
-  nsteps step n (σ1, m1) (σ2, m2) →
+    nsteps step n (σ1, m1) (σ2, m2) →
     (∀ `{Hinv : invGS_gen HasNoLc Σ},
-      ⊢ |={⊤}=> ∃
+      ⊢ |={⊤}=> (∃
          (φ' : val → iProp Σ),
-     let _ : osirisGS_gen Σ := OsirisG Σ Hinv Hstore
-     in
+     let _ : osirisGS_gen Σ := OsirisG Σ Hinv Hstore in
      state_interp σ1 ∗
-     wp s top m1 φ' ∗
+     wp s ⊤ m1 φ' ∗
      ( state_interp σ2 -∗
-       ⌜φ⌝)) →
+       wp s ⊤ m2 φ' -∗
+       |={⊤,∅}=> ⌜φ⌝))) →
   φ.
   Proof.
     intros Hsteps H.
@@ -524,89 +524,60 @@ Section Adequacy.
     destruct (is_ret m2) eqn:E2.
     { rewrite (invert_is_ret_Some E2).
       rewrite !wp_unfold/wp_pre/=.
-      iMod ("Hwp" with "[$]") as ">[??]".
-      iPoseProof (@fupd_mask_subseteq _ _ top ∅) as ">Hmod"; first done;
-        iModIntro.
-      iApply ("Himpl" with "[$]"). }
-    { iApply "Himpl".
-      iFrame.
-      by iPoseProof (@fupd_mask_subseteq _ _ top ∅) as ">Hmod"; first done. }
+      iMod ("Hwp" with "[$]") as ">[? H]".
+      iApply ("Himpl" with "[$][$H]").
+      { iIntros(?)"$". iApply fupd_mask_intro; first done.
+        iIntros "$". } }
+    { iApply ("Himpl" with "[$][$]"). }
   Qed.
 
-End Adequacy.
+  Lemma wp_adequacy' Σ `{!invGpreS Σ}
+        `{Hstore: @gen_heapGS loc val Σ loc_eq_decision loc_countable}
+        m σ n σ' m' {φ : val → Prop} :
+    nsteps step n (σ, m) (σ', m') →
+    (∀ `{Hinv : invGS_gen HasNoLc Σ},
+       ⊢ |={⊤}=>
+       let _ : osirisGS_gen Σ := OsirisG Σ Hinv Hstore in
+       (state_interp σ ∗
+        wp NotStuck ⊤ m (λ a, ⌜ φ a ⌝))) →
+    (¬ stuck (σ', m')) ∧ ∀ a, m' = Ret a → φ a.
+  Proof.
+    intros Hsteps H.
+    set φ' := match m' with
+              | ret v => φ v
+              | _ => can_step (σ', m')
+              end.
 
-(* Adequacy.
-
-Lemma wp_adequate {A} {σ} {m : free A} s E {φ} :
-  state_interp σ -∗
-  WP m @ s; E {{ λ a, ⌜ φ a ⌝ }} -∗
-  |={E,∅}=> ⌜ ∀ a, m = ret a → φ a ⌝.
-Proof.
-  iIntros "Hsi Hwp".
-  wp_unfold m. spec_state "Hwp".
-  wp_case_is_ret m Hret; [ destruct_wp_ret | destruct_wp_nonret ].
-  (* Case: [m] is [ret a]. *)
-  { iDestruct "Hwp" as "%". iPureIntro. intros a' ?. congruence. }
-  (* Case: [m] can step. *)
-  { iPureIntro. intros ? ->. simpl in *. congruence. }
-Qed.
-
-Local Open Scope nat_scope. (* TODO *)
-
-(* TODO this iterated modality should exist somewhere in the Iris library? *)
-
-Fixpoint tonight n (P : iProp Σ) :=
-  match n with
-  | 0 => P
-  | S n => |==> ▷ (tonight n P)
-  end%I.
-
-Lemma now_tonight (P : iProp Σ) :
-  ∀ n, P -∗ tonight n P.
-Proof.
-  induction n; simpl.
-  { eauto. }
-  { iIntros "H". iModIntro. iNext. iApply (IHn with "H"). }
-Qed.
-
-(* A combination of preservation, progress, and adequacy. *)
-
-Lemma wp_steps {A n σ σ'} {m m' : free A} s E {φ : A → Prop} :
-  steps n (σ, m) (σ', m') →
-  state_interp σ -∗
-  WP m @ s; E {{ a, ⌜ φ a ⌝ }} -∗
-  tonight n
-  ⌜ ¬ stuck (σ', m') ∧ ∀ a, m' = Ret a → φ a ⌝.
-Proof.
-  intros Hsteps.
-  remember (σ, m) as c eqn:Hc.
-  remember (σ', m') as c' eqn:Hc'.
-  revert n c c' Hsteps σ m σ' m' Hc Hc'.
-  induction 1; intros; simplify_eq; iIntros "Hsi Hwp".
-  (* Case: zero steps are taken. *)
-  { iDestruct (wp_not_stuck with "Hsi Hwp") as "%".
-    iDestruct (wp_adequate with "Hsi Hwp") as "%".
-    iApply now_tonight. iPureIntro. tauto. }
-  (* Case: [n] is nonzero. *)
-  { (* [m] steps to [m1]. *)
-    match goal with h: step _ ?m |- _ => destruct m as [σ1 m1] end.
-    specialize (IHHsteps _ _ _ _ eq_refl eq_refl).
-    simpl.
-    (* [WP m ...] implies [WP m1 ...]. *)
-    iDestruct (wp_step with "Hsi Hwp") as ">[Hsi Hwp]".
-    { eassumption. }
-    (* Eliminate two modalities. *)
-    iModIntro. iNext.
-    (* The induction hypothesis can then be exploited. *)
-    iApply (IHHsteps with "Hsi Hwp"). }
-Qed.
+    assert (φ' → ¬ stuck (σ', m') ∧ (∀ a : val, m' = ret a → φ a)) as Himpl.
+    { intros Hφ.
+      split.
+      { intros ?.
+        destruct m'; eauto using invert_stuck_ret, can_step_not_stuck. }
+      by intros ?->. }
+    apply Himpl.
+    eapply (wp_adequacy _ NotStuck m σ n σ' m' φ' Hsteps).
+    iIntros (Hinv).
+    iExists (λ v, ⌜φ v⌝)%I.
+    iMod H as "[$$]".
+    iModIntro.
+    iIntros "Hsi Hwp".
+    destruct m'; wp_unfold_all.
+    { iMod ("Hwp" with "[$]") as ">[_$]".
+      iApply fupd_mask_intro; [ done | by iIntros "_" ]. }
+    { iMod ("Hwp" with "[$]") as "[%?]"; exfalso.
+      eauto using invert_can_step_Crash. }
+    { iMod ("Hwp" with "[$]") as "[%Himp?]"; exfalso.
+      eauto using invert_can_step_Next. }
+    { by iMod ("Hwp" with "[$]") as "[%?]". }
+    { by iMod ("Hwp" with "[$]") as "[%?]". }
+  Qed.
 
 (* TODO is this lemma useful? If so, move it to safe.v where it belongs. *)
 
 Lemma prove_initially_safe {A} :
-  ∀ n σ {m : free A} {φ : A → Prop},
+  ∀ (n: nat) σ {m : free A} {φ : A → Prop},
   (
-    ∀ j σ' m',
+    ∀ (j : nat) σ' m',
     j ≤ n →
     steps j (σ, m) (σ', m') →
     ¬ stuck (σ', m') ∧ ∀ a, m' = Ret a → φ a
@@ -620,7 +591,7 @@ Proof.
   (* Case: [m] is [ret a]. *)
   { eapply initially_safe_ret.
     (* The goal is now [φ a]. *)
-    eapply (H 0 σ (ret a)); eauto with lia steps. }
+    eapply (H O σ (ret a)); eauto with lia steps. }
 
   (* Case: [(σ, m)] can step. *)
   { right. split; [ eauto |]. intros (σ', m') Hstep.
@@ -629,7 +600,7 @@ Proof.
     eapply (H (S j)); eauto with lia steps. }
 
   (* Case: [(σ, m)] is stuck. *)
-  { exfalso. eapply (H 0); eauto with lia steps. }
+  { exfalso. eapply (H O); eauto with lia steps. }
 
 Qed.
 
@@ -644,21 +615,5 @@ Proof.
   unfold safe. eauto using prove_initially_safe.
 Qed.
 
-(* TODO where do we go from here? *) *)
-
-(* -------------------------------------------------------------------------- *)
-
-(* The parallel composition rule of Separation Logic. *)
-
-(* To prove that [Par m1 m2 k ko] satisfies [φ], one must provide
-   two postconditions [φ1] and [φ2] and *separately* prove that:
-   - [m1] satisfies [φ1]
-   - [m2] satisfies [φ2]
-   - for all results [a1] and [a2] that satisfy [φ1] and [φ2],
-     the application of the the continuation [k]
-     to the pair [(a1, a2)] satisfies [φ]. *)
-
-(* The lemma could be strengthened by placing a ▷ modality in front of the
-   third premise, but I doubt that this would be useful, so I remove it.
-   We do not want the user to rely on the fact that a join point counts
-   as a step. *)
+(* TODO where do we go from here? *)
+End Adequacy.
