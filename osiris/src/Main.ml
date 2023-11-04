552de737d111c37fdbc22c63f2794575d91c936e
François Pottier <francois.pottier@inria.fr>
2023-11-04 14:27:28 +0100

    Cleanup.

--- osiris/src/Main.ml
+++ osiris/src/Main.ml
@@ -8,11 +8,13 @@ open SysExtra
 
 (* -------------------------------------------------------------------------- *)
 
+(* Reading a .cmt file and extracting the typed tree. *)
+
+(* https://github.com/ocaml/ocaml/blob/trunk/file_formats/cmt_format.mli *)
+
 let read_cmt cmt_file : Typedtree.structure =
   assert (is_absolute cmt_file);
-  match
-    Cmt_format.read_cmt cmt_file
-  with
+  match Cmt_format.read_cmt cmt_file with
   | exception Cmt_format.(Error (Not_a_typedtree _))
   | exception Cmi_format.(Error (Not_an_interface _)) ->
       fail "Invalid .cmt file: %s\n" cmt_file
