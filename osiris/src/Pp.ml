8bd881605c18c520c5ebfe585f75bf84096c784e
François Pottier <francois.pottier@inria.fr>
2023-11-06 23:19:35 +0100

    Some improvements in the pretty-printer.

--- osiris/src/Pp.ml
+++ osiris/src/Pp.ml
@@ -6,7 +6,7 @@ open Coq
 (* A block with indentation. *)
 
 let block opening contents closing =
-  group (opening ^^ nest 2 (contents) ^^ closing)
+  group (opening ^^ nest 2 contents ^^ closing)
 
 (* -------------------------------------------------------------------------- *)
 
@@ -23,38 +23,54 @@ let parens d =
 
 (* -------------------------------------------------------------------------- *)
 
+let semibreak =
+  semi ^^ break 1
+
+let commabreak =
+  comma ^^ break 1
+
+let lbracket, rbracket =
+  lbracket ^^ break 1,
+  break 1 ^^ rbracket
+
+let brackets doc =
+  nest 2 (lbracket ^^ doc) ^^ rbracket
+
 let rec pretty_printer (expr : expression) =
-  match expr with
-  | CAtom s -> string s
-  | CCon (c, []) -> string c
+  group @@ match expr with
+  | CAtom s ->
+      string s
+  | CCon (c, []) ->
+      string c
   | CCon (c, es) ->
-     parens (
-         string c ^^ space ^^
-           separate_map (break 1) pretty_printer es
-       )
+      parens (
+        string c ^^ space ^^
+        separate_map (break 1) pretty_printer es
+      )
   | CList [] ->
       string "[]"
   | CList es ->
       brackets (
-        separate_map semi pretty_printer es
+        separate_map semibreak pretty_printer es
       )
   | CTuple [] ->
       string "()"
   | CTuple es ->
       parens (
-        separate_map comma pretty_printer es
+        separate_map commabreak pretty_printer es
       )
   | CMark e ->
       (* A remaining mark is ignored. *)
       pretty_printer e
 
 let print_def def =
-  string "Definition " ^^ string def.lhs ^^ string " :="
-  ^^ hardline
-  ^^ group (pretty_printer def.rhs)
-  ^^ dot
-  ^^ hardline
-  ^^ hardline
+  string "Definition " ^^ string def.lhs ^^ string " :=" ^^ nest 2 (group (
+    break 1 ^^
+    pretty_printer def.rhs ^^
+    dot
+  )) ^^
+  hardline ^^
+  hardline
 
 let print_defs defs =
   concat_map print_def defs
