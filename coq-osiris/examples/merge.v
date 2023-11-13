a17c9d5f9a570227640e87fd39766b9af182ccac
Remyjck <remy.seassau@gmail.com>
2023-11-13 17:47:50 +0100

    Started proving merge2

--- coq-osiris/examples/merge.v
+++ coq-osiris/examples/merge.v
@@ -97,28 +97,26 @@ Proof.
 Qed.
 Local Hint Resolve Zle_Transitive : core.
 
-Definition is_env_with_spec name (spec : val -> Prop) :=
-  fun v =>
-    match v with
-    | VStruct env =>
-        match (lookup_name env name) with
-        | ret v' => spec v'
-        | _ => False
-        end
-    | _ => False
-    end.
-
-Lemma Merge__spec:
-  let η := EnvCons "Stdlib" Stdlib Stdlib_env in
-  SIMP (eval_mexpr η __main)
-       (is_env_with_spec "merge" merge_spec).
+Lemma Sorted_decomp {A} (cmp : A -> A -> Prop) `{Transitive A cmp} l l' x h t :
+    Sorted cmp l -> (h :: t) ++ l'  ≡ₚ l ->
+    Sorted cmp l' -> Sorted cmp (h :: t) ->
+    HdRel cmp x l' -> cmp x h ->
+    Sorted cmp (x :: l).
 Proof.
   intros.
-  eapply SIMP_simp; [eapply advance_simp_bind_bind; eapply advance_SimpBind ;simp_close |].
-  simpl.
-  (* Because we don't use the spec once we prove it, we can omit the use of SIMP_specify *)
-  SIMP_continue.
-  fixme.
+  apply Sorted_cons; first assumption.
+  (* Goal: HdRel cmp x l *)
+  apply Sorted_Hd_cmp; only 1-2: auto.
+  (* Goal: Forall (cmp x) l *)
+  rewrite <- H1; apply Forall_app.
+  split; apply Sorted_Hd_cmp; auto.
+Qed.
+
+Lemma Merge_spec:
+  let η := EnvCons "Stdlib" Stdlib Stdlib_env in
+  merge_spec (VCloRec η __bindings4 "merge").
+Proof.
+  intros.
   generalize η; clear η; intro η.
   unfold merge_spec.
   induction l1 as [|h1 t1]; intros.
@@ -132,7 +130,8 @@ Proof.
   destruct IHt1 as (h1l2&IH1&?&?); auto.
   destruct IHt2 as (l1t2&IH2&?&?); auto.
   SIMP1. SIMP_continue. SIMP_continue.
-  destruct (lt _) eqn:branch; simpl.
+  destruct (lt _) eqn:branch; simpl;
+    rewrite lt_repr_repr in branch; try auto.
   { unfold __exp1. fixme.
     (* Advance to call merge *)
     eapply SIMP_simp.
@@ -144,19 +143,13 @@ Proof.
     SimpRet.
     repeat (eapply advance_SimpBind; simpl; first SimpRet).
     eapply advance_SimpBind; simpl.
+
     (* Use induction hypothesis *)
     eapply prove_simp_try. { apply IH2. }
     all: try apply SimpReflexive; simpl.
     SIMP_ret.
     split.
-    { (* Goal: Sorted (h2 :: l1t2) *)
-      apply Sorted_cons; first assumption.
-      apply Sorted_Hd_cmp; try auto.
-      (* Goal: Forall (le h2) l1t2 *)
-      rewrite <- H9; apply Forall_app.
-      split; apply Sorted_Hd_cmp; auto.
-      constructor.
-      rewrite lt_repr_repr in branch; only 2-3: auto.
+    { eapply Sorted_decomp; eauto.
       apply Z.ltb_lt in branch.
       by apply Z.lt_le_incl. }
     { (* (h1 :: t1) ++ h2 :: t2 =p h2 :: l1t2 *)
@@ -183,16 +176,110 @@ Proof.
     SIMP_ret.
     split.
     { (* Goal: Sorted le (h1 :: h1l2) *)
-      apply Sorted_cons; first assumption.
-      apply Sorted_Hd_cmp; try auto.
-      (* Goal: Forall (le h1) h1l2 *)
-      rewrite <- H5; apply Forall_app.
-      split; apply Sorted_Hd_cmp; auto.
-      apply HdRel_cons.
+      rewrite Permutation_app_comm in H5.
+      eapply Sorted_decomp; eauto.
       (* Goal: h1 <= h2 *)
-      rewrite lt_repr_repr in branch; only 2-3: auto.
       by apply Z.ltb_ge in branch. }
     { (* (h1  :: t1) ++ h2 :: t2 =p h1 :: h1l2 *)
       rewrite <- app_comm_cons.
       by apply Permutation_skip. }}
 Qed.
+
+Definition merge2_spec (merge2 : val) : Prop :=
+  ∀ A `(_ : Encode A) (l1 l2 : list Z),
+    Forall (fun n => representable n) l1 ->
+    Forall (fun n => representable n) l2 ->
+    Sorted (Z.le) l1 ->
+    Sorted (Z.le) l2 ->
+    SIMP
+      (c ← call merge2 #l1;
+       call c #l2)
+      (λ l, Sorted (Z.le) l /\ Permutation (l1 ++ l2) l).
+
+Lemma Merge2_spec η:
+  merge2_spec (VCloRec η
+                 (RecBiCons
+                    (RecBinding "merge2" (AnonFun "l1" (EAnonFun __fun8))) RecBiNil)
+                 "merge2").
+Proof.
+  unfold merge2_spec.
+  induction l1 as [|h1 t1]; intros.
+  { rewrite app_nil_l.
+    SIMP1; repeat SIMP_continue. auto. }
+  induction l2 as [|h2 t2].
+  { rewrite app_nil_r.
+    SIMP1; repeat SIMP_continue. auto. }
+  specialize (IHt1 (h2::t2)).
+  inversion H0; inversion H1; inversion H2; inversion H3; subst.
+  destruct IHt1 as (h1l2&IH1&?&?); auto.
+  destruct IHt2 as (l1t2&IH2&?&?); auto.
+  SIMP1. SIMP_continue.
+  destruct (lt _) eqn:branch; simpl;
+    rewrite lt_repr_repr in branch; try auto.
+  { unfold __exp6. fixme.
+    (* Advance to call merge *)
+    eapply SIMP_simp.
+    simp_eval. eapply advance_SimpBind.
+    apply advance_simp_eval. cbn [eval'].
+    eapply advance_SimpBind. cbn [evals].
+    eapply advance_SimpBind. simpl.
+    repeat (rewrite eval_eval'; simpl).
+    SimpRet.
+    repeat (eapply advance_SimpBind; simpl; first SimpRet).
+    eapply advance_SimpBind; simpl.
+
+
+    (* Use induction hypothesis *)
+    apply advance_simp_bind_as_try.
+    simpl in IH2.
+    Admitted.
+
+Definition is_env_with_spec name (spec : val -> Prop) :=
+  fun v =>
+    match v with
+    | VStruct env =>
+        match (lookup_name env name) with
+        | ret v' => spec v'
+        | _ => False
+        end
+    | _ => False
+    end.
+
+Fixpoint env_has_specs env (l : list (string * (val -> Prop))) :=
+  match l with
+  | [] => True
+  | [x] => let (name, spec) := x in
+          match (lookup_name env name) with
+          | ret v' => spec v'
+          | _ => False
+          end
+  | h::t => let (name, spec) := h in
+          match (lookup_name env name) with
+          | ret v' => spec v' /\ env_has_specs env t
+          | _ => False
+          end
+  end.
+
+Fixpoint is_env_with_specs (l : list (string * (val -> Prop))) :=
+  fun v => match v with
+        | VStruct env => env_has_specs env l
+        | _ => False
+        end.
+
+Lemma Merge__spec:
+  let η := EnvCons "Stdlib" Stdlib Stdlib_env in
+  SIMP (eval_mexpr η __main)
+    (is_env_with_specs [("merge", merge_spec);
+                        ("merge2", merge2_spec)]).
+Proof.
+  intros.
+  SIMP1.
+  SIMP_specify "merge" merge_spec.
+  { apply Merge_spec. } intros merge _spec.
+  SIMP_continue.
+  SIMP_specify "merge2" merge2_spec.
+  { admit. } intros merge2 _spec2.
+  repeat SIMP_continue.
+  (* Postcondition *)
+  simpl. auto.
+Admitted.
