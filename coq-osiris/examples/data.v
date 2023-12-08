7c12a3813861562a5072329c1700278903146d5e
François Pottier <francois.pottier@inria.fr>
2023-12-08 08:50:33 +0100

    New example (examples/data) with a use of [Obj.magic].

---
+++ coq-osiris/examples/data.v
@@ -0,0 +1,39 @@
+From osiris Require Import osiris.
+From osiris.examples Require Import og_data.
+
+(* -------------------------------------------------------------------------- *)
+
+(* TODO UNUSED
+(* Boilerplate: reflect the algebraic data type ['a arity0]. *)
+
+Inductive arity0 : Type :=
+| A0: arity0.
+
+Definition encode_arity0 (a : arity0) : val :=
+  match a with
+  | A0 =>
+      VConstant "A"
+  end.
+
+Local Instance Encode_arity0 : Encode arity0 :=
+  { encode := encode_arity0 }.
+
+Lemma encode_arity0_is_encode :
+  ∀ (a : arity0),
+  encode_arity0 a = #a.
+Proof. eauto. Qed.
+
+Local Hint Resolve encode_arity0_is_encode : encode.
+ *)
+
+(* -------------------------------------------------------------------------- *)
+
+(* This program crashes. *)
+
+Local Transparent structural_equality_error.
+
+Lemma main η :
+  simp (eval_mexpr η __main) crash.
+Proof.
+  simp. simp_continue. simp_continue.
+Qed.
