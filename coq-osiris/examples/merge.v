27dc1366f0cef738fb0c87d5756290ce3d07084a
Remyjck <remy.seassau@gmail.com>
2023-12-13 15:55:13 +0100

    Reworked hoare-style proof of split

--- coq-osiris/examples/merge.v
+++ coq-osiris/examples/merge.v
@@ -258,98 +258,77 @@ Proof.
   destruct l as [| a l]; last destruct l as [| b l].
   (* Case: l = [] *)
   { eapply pure_eval_match.
-    { apply pure_eval_path.
-      eapply pure_ret; first solve [encode].
+    { apply pure_eval_path; simpl.
+      pure_ret.
       reflexivity. }
     simpl.
     apply pure_eval_ret_concat.
-    eapply pure_eval_pair.
-    { eapply pure_eval_const; first solve [encode].
-      apply eq_refl. }
-    { eapply pure_eval_const; first solve [encode].
-      apply eq_refl. }
-    intros a1 a2 ??; subst a1; subst a2.
+    apply pure_eval_pair.
+    eapply pure_eval_const; first solve [encode].
+    eapply pure_eval_const; first solve [encode].
     by simpl. }
   (* Case: l = [a] *)
   { eapply pure_eval_match.
     { apply pure_eval_path; simpl.
-      eapply pure_ret; first solve [encode].
+      pure_ret.
       reflexivity. }
     simpl.
     apply pure_eval_ret_concat.
     eapply pure_eval_pair.
-    { eapply pure_eval_data.
-      { eapply pure_eval_pair.
-        { apply pure_eval_path; simpl.
-          eapply pure_ret; first reflexivity.
-          apply eq_refl. }
-        { eapply pure_eval_const.
-          { eapply solve_encode_Nil; reflexivity. }
-          apply eq_refl. }
-        intros a1 a2 ??; subst a1; subst a2.
-        reflexivity. }
-      eapply solve_encode_Cons; reflexivity.
-      apply eq_refl. }
-    { eapply pure_eval_const; first solve [encode].
-      apply eq_refl. }
-    intros a1 a2 ??; subst a1; subst a2.
+    eapply pure_eval_data.
+    { apply pure_eval_pair_val.
+      apply pure_eval_path; simpl.
+      pure_ret.
+      eapply pure_eval_const; first solve [encode].
+      eapply solve_encode_Cons.
+      - reflexivity.
+      - reflexivity.
+      - apply solve_encode_Nil; reflexivity. }
+    eapply pure_eval_const; first solve [encode].
     by simpl. }
   (* Case: l = a :: b :: l *)
-  specialize (IH l).
-  destruct IH as ([l1 l2] & IH & Hpost); auto with arith.
-  eapply pure_eval_match.
-  { apply pure_eval_path; simpl.
-    eapply pure_ret; first solve [encode].
-    reflexivity. }
-  simpl.
-  apply pure_eval_ret_concat.
-  eapply pure_eval_let_pair.
-  { eapply pure_eval_app.
+  { specialize (IH l).
+    destruct IH as ([l1 l2] & IH & Hpost); auto with arith.
+    eapply pure_eval_match.
     { apply pure_eval_path; simpl.
-      eapply pure_ret; first solve [encode].
-      apply eq_refl. }
-    { apply pure_eval_path; simpl.
-      eapply pure_ret; first solve [encode].
-      apply eq_refl. }
-    intros v1 v2 ??.
-    subst v1. subst v2.
-    eapply pure_simp.
-    apply IH.
-    eapply pure_ret; first solve [encode].
-    reflexivity. }
-  { apply SimpReflexive. }
-  unfold __exp5; simpl.
-  eapply pure_eval_pair.
-  { eapply pure_eval_data.
-    { eapply pure_eval_pair.
-      { apply pure_eval_path; simpl.
-        eapply pure_ret; first solve [encode].
-        apply eq_refl. }
-      { apply pure_eval_path; simpl.
-        eapply pure_ret; first solve [encode].
-        apply eq_refl. }
-      intros a1 a2 ??; subst a1; subst a2.
+      pure_ret.
       reflexivity. }
-    eapply solve_encode_Cons. reflexivity. reflexivity. reflexivity.
-    apply eq_refl. }
+    simpl.
+    apply pure_eval_ret_concat.
+    eapply pure_eval_let_pair.
+    { eapply pure_eval_app. (* Should not be so hard to use this lemma *)
+      { apply pure_eval_path; simpl.
+        pure_ret. apply eq_refl. }
+      { apply pure_eval_path; simpl.
+        pure_ret. apply eq_refl. }
+      intros v1 v2 ??.
+      subst v1. subst v2.
+      eapply pure_simp.
+      apply IH.
+      pure_ret.
+      reflexivity. }
+    { simpl. apply SimpReflexive. }
+    unfold __exp5; simpl.
+    apply pure_eval_pair.
+    eapply pure_eval_data.
+    { apply pure_eval_pair_val.
+      apply pure_eval_path; simpl.
+      pure_ret.
+      apply pure_eval_path; simpl.
+      pure_ret.
+      eapply solve_encode_Cons; reflexivity. }
   eapply pure_eval_data.
-  { eapply pure_eval_pair.
-    { apply pure_eval_path; simpl.
-      eapply pure_ret; first solve [encode].
-      apply eq_refl. }
-    { apply pure_eval_path; simpl.
-      eapply pure_ret; first solve [encode].
-      apply eq_refl. }
-    intros a1 a2 ??; subst a1; subst a2.
-    reflexivity. }
-  eapply solve_encode_Cons. reflexivity. reflexivity. reflexivity.
-  apply eq_refl.
-  intros al1 bl2 ??; subst al1; subst bl2.
+  { apply pure_eval_pair_val.
+    apply pure_eval_path; simpl.
+    pure_ret.
+    apply pure_eval_path; simpl.
+    pure_ret.
+    eapply solve_encode_Cons; reflexivity. }
   unfold split_post in *; simpl in *.
   repeat destruct_hyp.
   split; last split.
   { (* Subgoal: the length of l1 is half the length of l *)
-      destruct (Nat.even _); eauto with arith. }
+    destruct (Nat.even _); eauto with arith. }
   { (* Subgoal: the length of l2 is hald the length of l *)
     eauto with arith. }
   { (* Subgoal: l1++l2 is a permutation of l *)
@@ -357,7 +336,7 @@ Proof.
     change ((a::l1)++b::l2) with (a::l1++b::l2).
     apply Permutation_skip.
     apply Permutation_sym.
-    apply Permutation_middle. }
+    apply Permutation_middle. } }
   Unshelve.
   - apply A.
   - apply H.
