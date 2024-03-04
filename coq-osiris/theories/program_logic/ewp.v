3e7886480e87c0368aa03474c9f7cdd5b98ad1df
Irene Yoon <euisuny@cis.upenn.edu>
2024-03-04 16:45:49 +0100

    Some cleaning

--- coq-osiris/theories/program_logic/ewp.v
+++ coq-osiris/theories/program_logic/ewp.v
@@ -279,15 +279,18 @@ Notation "ψ ⤉ " := (lift_exn_spec ψ) (at level 30).
 Notation "'|' 'RET' x '=>' e ';' '|' 'EXN' y '=>' f " :=
   (ilift (fun x => e) (fun y => f))
     (at level 200, right associativity, format
-    "'[v ' '['  '|'  'RET'  x  '=>'  e ';' ']' '/' '[' '|'  'EXN'  y  '=>'  f ']' ']'").
+    "'[v ' '['  '|'  'RET'  x  '=>'  e ';' ']' '/'
+      '[' '|'  'EXN'  y  '=>'  f ']' ']'").
 
 (* Custom notation for hoare triples which state a postcondition only over the
     return continuation *)
 Notation "'WP' e @ E  <| Ψ '|' '>' {{ 'RET' v , Q } }" :=
   (ewp_def E e%E Ψ (lift_ret_spec (λ v, Q)))
     (at level 20, e, Q at level 200,
-      format "'[hv' 'WP'  e  '/' @  '[' '/' E  ']' '/' <| Ψ '|' '>' {{  '[' 'RET'  v ,  '/' Q  ']' } } ']'") : bi_scope.
+      format "'[hv' 'WP'  e  '/' @  '[' '/' E  ']' '/' <| Ψ '|' '>'
+        {{  '[' 'RET'  v ,  '/' Q  ']' } } ']'") : bi_scope.
 Notation "'WP' e <| Ψ '|' '>' {{ 'RET' v , Q } }" :=
   (ewp_def NotStuck ⊤ e%E Ψ (lift_ret_spec (λ v, Q)))
     (at level 20, e, Q at level 200,
-      format "'[hv' 'WP'  e  '/' <| Ψ '|' '>' {{  '[' 'RET'  v ,  '/' Q  ']' } } ']'") : bi_scope.
+      format "'[hv' 'WP'  e  '/' <| Ψ '|' '>'
+          {{  '[' 'RET'  v ,  '/' Q  ']' } } ']'") : bi_scope.
