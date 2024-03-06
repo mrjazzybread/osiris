b72d1f2f0bc955c9f38061e8d1db7990597e14ff
Remyjck <remy.seassau@gmail.com>
2024-03-06 18:08:53 +0100

    Implemented 2-switch make system, updated translator

--- osiris/src/Translate.ml
+++ osiris/src/Translate.ml
@@ -41,6 +41,12 @@ let rec show_path (p : Path.t) =
       sprintf "%s.%s" (show_path p) x
   | Papply (p1, p2) ->
       sprintf "%s(%s)" (show_path p1) (show_path p2)
+  | Pextra_ty (p1, p2) ->
+      match p2 with
+      | Pcstr_ty x ->
+	  sprintf "%s{%s}" x (show_path p1)
+      | Pext_ty ->
+	  sprintf "+=%s" (show_path p1)
 
 (* -------------------------------------------------------------------------- *)
 
@@ -425,14 +431,10 @@ let rec translate_expr (e: expression) : expr =
   | Texp_letexception _ ->
       eunsupported loc "let exception"
 
-  | Texp_assert
-      { exp_desc = Texp_construct ({ txt = Lident "false"; _}, _, []); _ }
-  | Texp_unreachable ->
-      (* [assert false] and [.] are both translated to [EAssertFalse]. *)
-      EAssertFalse
-
-  | Texp_assert e ->
-      EAssert (translate_expr e)
+  | Texp_assert (e, _) ->
+      (match (e.exp_desc) with
+      | Texp_construct ({ txt = Lident "false"; _}, _, []) -> EAssertFalse
+      | _ -> EAssert (translate_expr e))
 
   | Texp_lazy _ ->
       eunsupported loc "lazy"
@@ -445,6 +447,10 @@ let rec translate_expr (e: expression) : expr =
 
   | Texp_letop _ ->
       eunsupported loc "let operators"
+
+  | Texp_unreachable ->
+      (* [assert false] and [.] are both translated to [EAssertFalse]. *)
+      EAssertFalse
 
   | Texp_extension_constructor _ ->
       eunsupported loc "extension constructors"
@@ -717,7 +723,7 @@ and translate_record_field_def (label_desc, label_def) : fexpr option =
       None
   | Overridden (id, e) ->
       (* This field is defined. *)
-      Some ( 
+      Some (
         Fexpr (
           translate_record_field id label_desc,
           translate_expr e)
@@ -819,6 +825,9 @@ and translate_mod_expr (me : module_expr) : mexpr =
 
   | Tmod_apply _ ->
       munsupported loc "functor application"
+
+  | Tmod_apply_unit _ ->
+      munsupported loc "functor unit application"
 
   | Tmod_constraint _ ->
       munsupported loc "signature ascription"
