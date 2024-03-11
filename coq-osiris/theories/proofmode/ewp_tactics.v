401b60671ef5bf12a7f58a7957353f5afb590092
Irene Yoon <euisuny@cis.upenn.edu>
2024-03-11 10:49:57 +0100

    Simple ewp tactics

---
+++ coq-osiris/theories/proofmode/ewp_tactics.v
@@ -0,0 +1,124 @@
+From iris.proofmode Require Import base proofmode classes environments.
+From iris.base_logic.lib Require Import fancy_updates.
+From iris.bi Require Import weakestpre.
+From iris.prelude Require Import options.
+Import uPred.
+
+From iris Require Import base_logic.lib.gen_heap.
+
+From osiris Require Import base.
+From osiris.lang Require Import lang.
+From osiris.semantics Require Import semantics.
+From osiris.program_logic Require Import program_logic.
+From osiris.proofmode Require Import simp specifications.
+
+(** *Utility *)
+
+Create HintDb osiris.
+
+(* Some hints used for resolving [repr] expressions *)
+Global Hint Rewrite eq_repr_repr sub_repr_repr mul_repr_repr: osiris.
+Global Hint Extern 1 (representable _) => representable : osiris.
+Global Hint Unfold val_as_int : osiris.
+
+(** *"WP" tactics
+
+  These tactics operate on [WP] goals are Hoare-style;
+
+  All tactics that are not "Hoare-style" are internal and should not be exposed
+  to the user. *)
+
+(** *General tactics *)
+(* Start proof mode. *)
+Ltac Start_proof := iStartProof.
+
+(* Try to simplify the goal using the [simp] relation *)
+Ltac Simp := iApply ewp_simp; first try solve [simp].
+
+(* Hoare-style rules that correspond to [rule] lemmas *)
+Ltac Try := iApply ewp_try.
+
+Ltac Ret := iApply ewp_value.
+
+Ltac Par :=
+  lazymatch goal with
+  | |- envs_entails _ (ewp_def _ (Par (ret _) (ret _) _ _) _ _) =>
+      iApply ewp_simp; first simp
+  | |- envs_entails _ (ewp_def _ (Par _ (Ret _) _ _) _ _) =>
+      iApply ewp_simp; first simp
+  | |- envs_entails _ (ewp_def _ (Par (ret _) _ _ _) _ _) =>
+      iApply ewp_simp; first simp
+  | |- envs_entails _ (ewp_def _ (Par _ _ _ _) _ _) =>
+      iApply ewp_par
+  | _ => fail "The goal must be a par to apply [ewp_par]."
+  end; try done.
+
+(* -------------------------------------------------------------------------- *)
+Ltac Bind := iApply ewp_bind.
+
+(* If anything has been added to the hint data base of [osiris], using
+    [Hint Rewrite] or [Hint Extern], then apply the automation. *)
+Ltac Auto :=
+  autorewrite with osiris;
+  auto with osiris.
+
+(* [EWP normal form]
+    An assertion that checks whether the current goal is in normal form, i.e.
+    of the form ewp [..] *)
+(* Turn expression to normal form; (i.e. apply monadic unit/associativity
+  properties) *)
+Ltac Normalize :=
+  (* Normalize monadic [bind] and [ret] *)
+  cbn [bind];
+  repeat
+  match goal with
+  | |- envs_entails _ (ewp_def _ (Par _ _ _ _) _ _) => Par
+  | |- envs_entails _ (ewp_def _ (bind _ _) _ _) => Bind
+  | |- envs_entails _ (ewp_def _ (ret _) _ _) => Ret
+  end;
+  Auto.
+
+(* -------------------------------------------------------------------------- *)
+(* If the branch guard in [if _ then _ else _] can be resolved, we can decide
+   which branch to take. *)
+Ltac IfThenElse :=
+  match goal with
+  | |- envs_entails _ (ewp_def _ (if ?b then _ else _) _ _) =>
+      try (assert (b = false) as ->; first done);
+      try (assert (b = true) as ->; first done)
+  end.
+
+(* -------------------------------------------------------------------------- *)
+
+(* Store-related tactics. *)
+
+Ltac Alloc l H :=
+  iApply ewp_alloc; iNext; iIntros (l) H.
+Ltac Load H :=
+  iApply (ewp_load with H); iNext.
+Ltac Store H :=
+  iApply (ewp_store with H); iNext.
+
+(* -------------------------------------------------------------------------- *)
+
+(* Apply a [ewp] tactic; maybe we want to add in more checks or normalization
+  here *)
+Tactic Notation "ewp:" tactic(H) := try H.
+
+(* -------------------------------------------------------------------------- *)
+
+(** *Automation *)
+(* Extending the [by] and [done] tactics to solve trivial things in proof mode *)
+
+(* Useful for goals of the shape (⊢ ∀ (e : void), _), _.) which come up when
+  reasoning about exceptional continuations *)
+Global Hint Extern 0 (envs_entails _ (bi_forall (fun _ : void => _))) => iIntros (?)
+; try done : core.
+
+(* Apply general tactics *)
+Global Hint Extern 0 (envs_entails _ (ewp_def _ (ret _) _ _)) => ewp: Ret : core.
+
+(* Extra notation for proofmode (FIXME: Move to notations?) *)
+Arguments eval.eval {_}.
+
+Notation "OCAML⟦ x ⟧" := (eval.eval (deco x _)).
