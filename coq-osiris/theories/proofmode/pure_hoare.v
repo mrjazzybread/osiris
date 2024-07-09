132059b4022f7aa7fa156c6e03ba6c3fb051ca96
Remyjck <remy.seassau@gmail.com>
2024-07-09 15:47:37 +0200

    Better lemmas for recursive calls and letrec, following [expr_rules]

--- coq-osiris/theories/proofmode/pure_hoare.v
+++ coq-osiris/theories/proofmode/pure_hoare.v
@@ -261,281 +261,85 @@ Proof.
     ((exfalso; by eapply invert_pure_crash) || eauto).
 Qed.
 
-(* Replace the first occurence of a var in an environment *)
-
-Fixpoint replace_env_binding η (name : var) (x : val) {struct η} :=
-  match η with
-  | [] => []
-  | (name0, val0) :: η0 =>
-      if (name =? name0)%string then (name, x) :: η0 else
-        (name0, val0) :: (replace_env_binding η0 name x)
-  end.
-
-Lemma lookup_rec_bindings_cases rbs g :
-  lookup_rec_bindings rbs g = missing_variable g \/ exists a, lookup_rec_bindings rbs g = ret a.
+Lemma pure_stop_eval {Y} `{Encode X} η e k (φ : X -> Prop) :
+  pure (try2 (eval η e) k) φ ->
+  @pure X _ Y (Stop CEval (η, e) k) φ.
 Proof.
-  induction rbs as [|[f a] ? IH]; first auto.
-  simpl. destruct (g =? f)%string; eauto.
+  intros.
+  eapply pure_simp; [ apply SimpEval | assumption ].
 Qed.
 
-Lemma in_bindings_in_eval_bindings rbs fname η :
-  (∃ afun, lookup_rec_bindings rbs fname = ret afun) ->
-  exists vf, lookup_name (eval_rec_bindings η rbs) fname = ret vf.
-Proof.
-  intros [afun Hlkp].
-  unfold eval_rec_bindings. generalize rbs at 1.
-  induction rbs as [|[??] rbs IHrbs]; first discriminate; intros.
-  simpl in *. destruct (fname =? f)%string; eauto.
-Qed.
+(* - [X] is the type of the argument.
+   - [Y] is the type of result.
+   - [a : A] is an auxiliary variable used to relate the pre and post. *)
 
-Lemma replace_env_idempotent η fname vf :
-  lookup_name η fname = ret vf -> replace_env_binding η fname vf = η.
-Proof.
-  intros Hlkp.
-  induction η as [| [fname' ?] η IHη]; first discriminate.
-  simpl in *. destruct (fname =? fname')%string eqn:name_eq.
-  { apply String.eqb_eq in name_eq as ->. injection Hlkp as ->. reflexivity. }
-  by rewrite IHη.
-Qed.
-
-Lemma replace_env_binding_concat1 η1 η2 fname v :
-  (exists vf, lookup_name η1 fname = ret vf) ->
-  replace_env_binding (η1 ++ η2) fname v = (replace_env_binding η1 fname v) ++ η2.
-Proof.
-  intros [vf Hlkp].
-  induction η1 as [| [fname' ?] η1 IHη1]; first discriminate.
-  simpl in *. destruct (fname =? fname')%string eqn:name_eq; first reflexivity.
-  by rewrite IHη1.
-Qed.
-
-Lemma eval_bindings_produces_eq η rbs rbs' fname :
-  lookup_name (eval_rec_bindings η rbs) fname = ret (VCloRec η rbs' fname) -> rbs = rbs'.
-Proof.
-  unfold eval_rec_bindings. generalize rbs at 2; intros rbs''.
-  induction rbs'' as [|[??] ??]; first discriminate; simpl.
-  by destruct (_ =? _)%string; [injection 1|].
-Qed.
-
-Lemma lookup_eval_bindings_aux rbs η fname afun :
-  lookup_rec_bindings rbs fname = ret afun ->
-  exists rbs', lookup_name (eval_rec_bindings η rbs) fname = ret (VCloRec η rbs' fname).
-Proof.
-  intros Hlkp. unfold eval_rec_bindings. generalize rbs at 1.
-  induction rbs as [|[fname' ?] rbs IHrbs]; first discriminate; intros.
-  simpl in *. destruct (fname =? fname')%string eqn:name_eq.
-  { apply String.eqb_eq in name_eq as ->. eauto. }
-  eauto.
-Qed.
-
-Corollary lookup_eval_bindings rbs η fname afun :
-  lookup_rec_bindings rbs fname = ret afun ->
-  lookup_name (eval_rec_bindings η rbs) fname = ret (VCloRec η rbs fname).
-Proof.
-  intros Hlkp. eapply lookup_eval_bindings_aux in Hlkp as [rbs' Hlkp].
-  replace rbs' with rbs in Hlkp by (eapply eval_bindings_produces_eq; eauto).
-  apply Hlkp.
-Qed.
-
-Lemma replace_binding_idempotent η rbs fname afun :
-  lookup_rec_bindings rbs fname = ret afun ->
-  (replace_env_binding
-     ((eval_rec_bindings η rbs) ++ η)
-     fname
-     (VCloRec η rbs fname) =
-     (eval_rec_bindings η rbs) ++ η).
-Proof.
-  intros Hlkp.
-  apply lookup_eval_bindings with (η:=η) in Hlkp.
-  rewrite replace_env_binding_concat1 by eauto.
-  rewrite replace_env_idempotent by eauto.
-  done.
-Qed.
-
-Lemma replace_binding η rbs fname afun :
-  lookup_rec_bindings rbs fname = ret afun ->
-  replace_env_binding (eval_rec_bindings η rbs) fname (VCloRec η rbs fname) =
-    eval_rec_bindings η rbs.
-Proof.
-  intros Hlkp.
-  apply lookup_eval_bindings with (η:=η) in Hlkp.
-  rewrite replace_env_idempotent; auto.
-Qed.
-
-(* Todo: write comment *)
-
-Lemma pure_rec_call `{Encode X} `{Encode Y}
-  (η : env) (rbs : list rec_binding) (fname : var) (v : X)
-  (P : X -> Prop) (φ : X -> Y -> Prop) (R : X -> X -> Prop) :
+Lemma pure_rec_call `{Encode X, Encode Y} {A}
+  (η : env) (f : var) arg e1 (a : A) (x : X)
+  (P : A -> X -> Prop) (φ : A -> Y -> Prop) (R : X -> X -> Prop) :
   well_founded R ->
-  P v ->
-  (forall (vf : val) v',
-      P v' ->
-      (forall v'',
-          P v'' ->
-          R v'' v' ->
-          pure (call vf #v'') (φ v'')) ->
-      pure ('afun ← lookup_rec_bindings rbs fname ;
-            let δ := replace_env_binding (eval_rec_bindings η rbs) fname vf in
-            let 'AnonFun x e := afun in
-            eval ((x, #v') :: δ ++ η) e
-        ) (φ v')) ->
-  pure (call (VCloRec η rbs fname) #v) (φ v).
+  P a x ->
+  (∀ vf x,
+      (∀ (a : A) (y : X), R y x -> P a y -> pure (call vf #y) (φ a)) ->
+      (∀ (a : A), pure (eval (arg ~> #x; f ~> vf; η) e1) (φ a))) ->
+  pure (call (VCloRec η [RecBinding f (AnonFun arg e1)] f) #x) (φ a).
 Proof.
-  intros Hwf HP Hrec.
-  induction v as [v IH] using (well_founded_induction Hwf); intros.
-  apply pure_enter_call_VCloRec; simpl in *.
-  destruct (lookup_rec_bindings_cases rbs fname) as [Hlkp|[afun Hlkp]].
-  { rewrite Hlkp in *; simpl in *.
-    eapply Hrec with (vf:=VInt int.zero); eauto. }
-  { specialize (Hrec (VCloRec η rbs fname) v HP).
-    erewrite replace_binding in Hrec by eauto.
-    simpl in *; rewrite !Hlkp in *; simpl in *.
-    unfold acall; destruct afun; apply pure_EvalRetThrow.
-    eapply Hrec; auto. }
+  intros Hwf HPx Hrec.
+  generalize dependent a.
+  induction x as [x IH] using (well_founded_induction Hwf); intros.
+  simpl; rewrite String.eqb_refl; apply pure_stop_eval; rewrite try2_ret_right.
+  apply Hrec. intros a2 y HR HPy.
+  apply IH; auto.
 Qed.
 
-Notation "fname 'to' afun" :=
-  (RecBinding fname afun) (at level 50).
+(* - [φ] is the toplevel specification.
+   - [φf] is the specification of the function [f]. *)
 
-Lemma pure_rec_call_unary `{Encode X} `{Encode Y}
-  (η : env) afun (fname : var) (v : X)
-  (P : X -> Prop) (φ : X -> Y -> Prop) (R : X -> X -> Prop) :
+Lemma pure_letrec `{Encode X, Encode Y} {A}
+  (η : env) (f arg : var) e1 e2 (φ : X -> Prop) (φf : A -> Y -> Prop)
+  (P : A -> X -> Prop) (R : X -> X -> Prop) :
   well_founded R ->
-  P v ->
-  (forall (vf : val) v',
-      P v' ->
-      (forall v'',
-          P v'' ->
-          R v'' v' ->
-          pure (call vf #v'') (φ v'')) ->
-      pure (let 'AnonFun x e := afun in
-            eval ((x, #v') :: (fname, vf) :: η) e) (φ v')) ->
-  pure (call (VCloRec η [RecBinding fname afun] fname) #v) (φ v).
+  (* Subgoal:
+     Assuming that any recursive call of [f] on a smaller argument
+     satisfies [φf], show that evaluating [e1] satisfies [φf]. *)
+  (∀ vf (x : X),
+      (∀ (a : A) (y : X), R y x -> P a y -> pure (call vf #y) (φf a)) ->
+      ∀ (a : A), pure (eval ((arg, #x) :: (f, vf) :: η) e1) (φf a)) ->
+  (* Subogal:
+     Proceed with the right hand of the [let rec],
+     assuming f satisfies its spec. *)
+  (∀ vf,
+      (∀ a x, P a x -> pure (call vf #x) (φf a)) ->
+      pure (eval ((f, vf) :: η) e2) φ) ->
+  pure (eval η (ELetRec [RecBinding f (AnonFun arg e1)] e2)) φ.
 Proof.
-(*   intros Hwf HP Hrec. *)
-(*   eapply pure_rec_call. *)
-(*   { apply Hwf. } *)
-(*   { apply HP. } *)
-(*   simpl; rewrite String.eqb_refl. apply Hrec. *)
-(* Qed. *)
-  intros Hwf HP Hrec.
-  induction v as [v IH] using (well_founded_induction Hwf); intros.
-  apply pure_enter_call_VCloRec; simpl; rewrite String.eqb_refl.
-  unfold acall; destruct afun; apply pure_EvalRetThrow.
-  eapply Hrec; auto.
+  intros Hwf He1 He2.
+  simpl_eval. apply He2. clear He2.
+  intros a y HPy.
+  eapply pure_rec_call; eauto.
 Qed.
 
-Lemma pure_rec_call_binary_mutual `{Encode X} `{Encode Y}
-  (η : env) (afun agun : anonfun) (gname fname : var) (v : X)
-  (P : X -> Prop) (φ : X -> Y -> Prop) (R : X -> X -> Prop) :
+Lemma pure_rec_call_no_pre `{Encode X} `{Encode Y}
+  (η : env) (f arg : var) e1 e2 (φ : X -> Prop) (φf : Y -> Prop)
+  (R : X -> X -> Prop) :
   well_founded R ->
-  P v ->
-  (forall (vf : val) v',
-      P v' ->
-      (forall v'',
-          P v'' ->
-          R v'' v' ->
-          pure (call vf #v'') (φ v'')) ->
-      pure (let 'AnonFun x e := afun in
-            eval (x ~> #v';
-                  fname ~> vf;
-                  gname ~> (VCloRec η [RecBinding fname afun; RecBinding gname agun] gname);
-                  η) e) (φ v')) ->
-  pure (call (VCloRec η [RecBinding fname afun; RecBinding gname agun] fname) #v) (φ v).
+  (* Subgoal:
+     Assuming that any recursive call of [f] on a smaller argument
+     satisfies [φf], show that evaluating [e1] satisfies [φf]. *)
+  (∀ vf (x : X),
+      (∀ (y : X), R y x -> pure (call vf #y) φf) ->
+      pure (eval ((arg, #x) :: (f, vf) :: η) e1) φf) ->
+  (* Subogal:
+     Proceed with the right hand of the [let rec],
+     assuming f satisfies its spec. *)
+  (∀ vf,
+      (∀ (x : X), pure (call vf #x) φf) ->
+      pure (eval ((f, vf) :: η) e2) φ) ->
+  pure (eval η (ELetRec [RecBinding f (AnonFun arg e1)] e2)) φ.
 Proof.
-(*   intros Hwf HP Hrec. *)
-(*   eapply pure_rec_call. *)
-(*   { apply Hwf. } *)
-(*   { apply HP. } *)
-(*   simpl; rewrite String.eqb_refl. apply Hrec. *)
-(* Qed. *)
-  intros Hwf HP Hrec.
-  induction v as [v IH] using (well_founded_induction Hwf); intros.
-  apply pure_enter_call_VCloRec; simpl; rewrite String.eqb_refl.
-  unfold acall; destruct afun; apply pure_EvalRetThrow.
-  eapply Hrec; auto.
-Qed.
-
-Lemma pure_rec_call_binary_mutual_aliasing `{Encode X} `{Encode Y}
-  (η : env) ef eg farg garg (gname fname : var) (v : X)
-  (P : X -> Prop) (φ : X -> Y -> Prop) (R : X -> X -> Prop) :
-  well_founded R ->
-  P v ->
-  (forall (vf : val) v',
-      P v' ->
-      (forall v'',
-          P v'' ->
-          R v'' v' ->
-          pure (call vf #v'') (φ v'')) ->
-      pure (eval ((if (fname =? gname)%string then garg else farg) ~> #v';
-                  gname ~>
-                    if (fname =? gname)%string
-                    then vf
-                    else (VCloRec
-                            η
-                            [RecBinding gname (AnonFun garg eg);
-                             RecBinding fname (AnonFun farg ef)]
-                            gname);
-                  fname ~>
-                    if (fname =? gname)%string
-                    then (VCloRec η
-                            [RecBinding gname (AnonFun garg eg);
-                             RecBinding fname (AnonFun farg ef)]
-                            fname)
-                    else vf;
-                  η) (if (fname =? gname)%string then eg else ef)) (φ v')) ->
-  pure (call (VCloRec η [RecBinding gname (AnonFun garg eg);
-                         RecBinding fname (AnonFun farg ef)] fname) #v) (φ v).
-Proof.
-(*   intros Hwf HP Hrec. *)
-(*   eapply pure_rec_call. *)
-(*   { apply Hwf. } *)
-(*   { apply HP. } *)
-(*   simpl; destruct (fname =? gname)%string eqn:name_eq; simpl. *)
-(*   { apply String.eqb_eq in name_eq. rewrite name_eq in *. apply Hrec. } *)
-(*   rewrite String.eqb_refl. apply Hrec. *)
-(* Qed. *)
-  intros Hwf HP Hrec.
-  induction v as [v IH] using (well_founded_induction Hwf); intros.
-  apply pure_enter_call_VCloRec; simpl; rewrite String.eqb_refl.
-  unfold acall; destruct (fname =? gname)%string eqn:name_eq; apply pure_EvalRetThrow.
-  { apply String.eqb_eq in name_eq as ->. eapply Hrec; eauto. }
-  eapply Hrec; eauto.
-Qed.
-
-Lemma pure_rec_call_binary_mutual2 `{Encode X} `{Encode Y}
-  (η : env) ef farg agun (gname fname : var) (v : X)
-  (P : X -> Prop) (φ : X -> Y -> Prop) (R : X -> X -> Prop) :
-  well_founded R ->
-  P v ->
-  (fname =? gname)%string = false ->
-  (forall (vf : val) v',
-      P v' ->
-      (forall v'',
-          P v'' ->
-          R v'' v' ->
-          pure (call vf #v'') (φ v'')) ->
-      pure (eval (farg ~> #v';
-                  gname ~>
-                    (VCloRec η [RecBinding gname agun;
-                                RecBinding fname (AnonFun farg ef)] gname);
-                  fname ~> vf;
-                  η) ef) (φ v')) ->
-  pure (call (VCloRec η [RecBinding gname agun;
-                         RecBinding fname (AnonFun farg ef)] fname) #v) (φ v).
-Proof.
-(*   intros Hwf HP Hname Hrec. *)
-(*   eapply pure_rec_call. *)
-(*   { apply Hwf. } *)
-(*   { apply HP. } *)
-(*   simpl. rewrite Hname. rewrite String.eqb_refl. simpl. *)
-(*   apply Hrec. *)
-  (* Qed. *)
-  intros Hwf HP Hname Hrec.
-  induction v as [v IH] using (well_founded_induction Hwf); intros.
-  apply pure_enter_call_VCloRec; simpl; rewrite String.eqb_refl.
-  unfold acall; rewrite Hname; apply pure_EvalRetThrow.
-  eapply Hrec; eauto.
+  intros Hwf He1 He2.
+  simpl_eval. apply He2. clear He2.
+  intros y. change φf with ((fun (x : unit) => φf) ()).
+  eapply pure_rec_call with (P := fun _ _ => True); eauto.
 Qed.
 
 Definition pure_call2 `{Encode X} vf arg1 arg2 (φ : X -> Prop) :=
@@ -579,42 +383,4 @@ Proof.
   apply (IH (v1'', v2'')); eauto. simpl.
   rewrite H2.
   eapply pure_ret; [ solve [encode] | reflexivity].
-Qed.
-
-Lemma pure_nested_call `{Encode A} `{Encode B} `{Encode C}
-  (η : env) rbs (fname : var) (v1 : A) (v2 : B) x y e1 e2
-  (P : A -> B -> Prop) (φ : A -> B -> C -> Prop) (R : (A * B) -> (A * B) -> Prop) :
-  let δ := eval_rec_bindings η rbs in
-  lookup_rec_bindings rbs fname = ret (AnonFun x e1) ->
-  (let η0 := (x ~> #v1; δ ++ η) in
-   eval η0 e1 = ret (VClo η0 (AnonFun y e2))) ->
-  well_founded R ->
-  P v1 v2 ->
-  (forall vf v1' v2',
-      P v1' v2' ->
-      (forall v1'' v2'',
-          (let η0 := (x ~> #v1''; δ ++ η) in
-           eval η0 e1 = ret (VClo η0 (AnonFun y e2))) ->
-          P v1'' v2'' ->
-          R (v1'', v2'') (v1', v2') ->
-          pure_call2 vf #v1'' #v2'' (φ v1'' v2'')) ->
-      let δ := replace_env_binding δ fname vf in
-      pure (eval (y ~> #v2'; x ~> #v1'; δ ++ η) e2) (φ v1' v2')) ->
-  pure_call2 (VCloRec η rbs fname) #v1 #v2 (φ v1 v2).
-Proof.
-  cbn zeta.
-  intros Hlkp Heval Hwf HP Hrec.
-  remember (v1, v2) as p eqn:Hpeq.
-  replace v1 with (p.1) in * by (rewrite Hpeq; reflexivity).
-  replace v2 with (p.2) in * by (rewrite Hpeq; reflexivity).
-  clear v1 v2 Hpeq.
-  induction p as [p IH] using (well_founded_induction Hwf); intros.
-  destruct p as [v1 v2]; simpl in *.
-  apply pure_enter_call_VCloRec; rewrite Hlkp; simpl.
-  apply pure_EvalRetThrow. rewrite Heval.
-  eapply pure_ret; first solve [encode].
-  apply pure_enter_call_VClo; apply pure_EvalRetThrow.
-  rewrite <- (replace_binding _ _ _ _ Hlkp).
-  eapply Hrec; auto; intros.
-  apply (IH (v1'', v2'')); auto.
 Qed.
