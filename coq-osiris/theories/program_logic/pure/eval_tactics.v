9e6262fb9ec878bea2e933700f76d5968988d086
Irene Yoon <euisuny@cis.upenn.edu>
2024-11-22 14:47:32 +0100

    Remove straggling file

--- coq-osiris/theories/program_logic/pure/eval_tactics.v
+++
@@ -1,4 +0,0 @@
-From osiris Require Import base.
-From osiris.lang Require Import lang.
-From osiris.semantics Require Import semantics.
-From osiris.program_logic.pure Require Import eval_rules pattern_rules.
