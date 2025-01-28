aaa8026018d9c95214d152f2863901d528cb313d
François Pottier <francois.pottier@inria.fr>
2025-01-28 13:50:00 +0100

    Remove a TODO.

--- coq-osiris/theories/semantics/outcome.v
+++ coq-osiris/theories/semantics/outcome.v
@@ -66,6 +66,3 @@ Definition discontinue {A E R} (k : outcome2 A E -> R) (e : E) : R :=
 
 Definition glue2 {A E R} (f : A -> R) (h : E -> R) : outcome2 A E -> R :=
   fun o => match o with O2Ret a => f a | O2Throw e => h e end.
-
-(* TODO prove lemmas about composing [continue] and [discontinue]
-   with [glue2]? *)
