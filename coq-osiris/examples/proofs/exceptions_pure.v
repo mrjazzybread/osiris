475264ec47730a9ed4d2b2bc7db622cf2e17bf24
Remy Seassau <remy.seassau@gmail.com>
2025-02-07 15:28:05 +0100

    Fix proof and expr_rules.

--- coq-osiris/examples/proofs/exceptions_pure.v
+++ coq-osiris/examples/proofs/exceptions_pure.v
@@ -1,7 +1,7 @@
 (* This file is a "pure mode" version of exceptions.v, to guide the
-  generalization of lemmas about [pure] to non-trivial exceptional postconditions,
-  but one should be aware that the current version of exceptions.v is old and
-  should not be considered idiomatic (for this see iter.v instead).
+generalization of lemmas about [pure] to non-trivial exceptional postconditions,
+but one should be aware that the current version of exceptions.v is old and
+should not be considered idiomatic (for this see iter.v instead).
 
 TODO: avoid [simp] at least when [eval ...] does not reduce to a value / exn *)
 
@@ -27,93 +27,36 @@ Definition head_spec head :=
 
 Definition catch_head_spec catch_head :=
   ∀ (A : Type) (H : Encode A) (l : list A),
-   pure
+    pure
       (call catch_head #l)
       (λ hopt, hopt = list.head l) ⊥.
 
-Lemma Head_spec :
-  pure (eval stdlib_with_notfound (EAnonFun __fun1)) head_spec ⊥.
-Proof.
-  pure_simp. unfold head_spec; intros.
+(* TODO: MOVE? *)
+Ltac set_pure_postcondition φ :=
+  match goal with
+    |- @pure ?A ?E ?m ?_φ ?ψ =>
+      let H := fresh in
+      cut (@pure A E m φ ψ); [ intro H; exact H | ]
+  end.
 
-  (* in both cases, the expr [simp]lifies to a value or exception *)
-  destruct l.
-  - eapply pure_wp_simp; first by simp_really. apply pure_wp_throw.
-    split; eauto.
-  - pure_simp.
-Qed.
-
-Lemma Catch_head_spec head :
-  head_spec head ->
-  pure
-    (eval (("head", head) :: stdlib_with_notfound)
-       (EAnonFun __fun3)) catch_head_spec bottom.
-Proof.
-  intros Hhead.
-  pure_simp; unfold catch_head_spec; intros.
-  pure_enter.
-  eapply pure_eval_match'_exn.
-  - eapply pure_simp. simp. apply Hhead.
-  - intros h (t & ->). pure_simp.
-  - intros e (-> & ->). pure_simp.
-Qed.
-
-Lemma Catch_head2_spec head chead:
-  head_spec head ->
-  (* FIXME *)
-  pure
-    (eval (("catch_head", chead) :: ("head", head) ::
-      stdlib_with_notfound)
-       (EAnonFun __fun5)) catch_head_spec bottom.
-Proof.
-  intros Hhead.
-  pure_simp; unfold catch_head_spec.
-  intros. pure_enter.
-  (* FIXME: This proof was not so pretty to begin with; but
-    we can do better here.
-
-    FIXME: Temporary proof for now; need to repair [pure_tactics]. *)
-  (* TODO: Refactor. *)
-  eapply (pure_eval_trywith _ _ _ _ (λ ex, l = [] ∧ ex = VXData (Loc 0) [])).
-  - (* matched value (or exn) *)
-    eapply pure_eval_data; last done. (* TODO Fix [pure_data] tactic. *)
-    eapply pure_ret_mono.
-    { eapply pure_evals.
-      apply Forall2_cons; split; last eapply Forall2_nil; eauto.
-      (* Application *)
-      eapply pure_eval_app.
-      pure_path. pure_path.
-      intros * -> -> ; eauto.
-      eapply pure_exn_mono; first eapply Hhead.
-      intros ? (?&?); eauto. }
-    { intros.
-      apply Forall2_cons_inv_l in H0.
-      destruct H0 as (?&?&?&?&?); subst.
-      cbn in *. destruct H0.
-      apply Forall2_nil_inv_l in H1; subst.
-      by cbn. }
-  - intros * (?&?); subst.
-    apply pure_eval_try_with_cons, pat_PXData_eq; auto.
-    ltac2: (patterns ()). pure_data. cbn.
-    exists (nil : list A); eauto. Unshelve. eauto.
-Qed.
-
-Lemma exceptions_pure__spec:
+Lemma example :
   eval_module stdlib_with_notfound __main (λ η, True).
 Proof.
+  unfold __main.
   apply module_struct.
-  next_item with head_spec.
-  { by apply Head_spec. }
 
-  intros [??] (head & Hhead & -> & ->).
-  next_item with catch_head_spec.
-  { by apply Catch_head_spec. }
+  (* Struct item: [let head l = ...] *)
+  eapply structs_cons.
+  { apply struct_let_single with (spec := head_spec).
+    pure_simp; unfold head_spec; intros.
 
-  intros [??] (chead & Hchead & -> & ->).
-  next_item with catch_head_spec.
-  { by apply Catch_head2_spec. }
+    (* in both cases, the expr [simp]lifies to a value or exception *)
+    destruct l.
+    - eapply pure_wp_simp; first by simp_really. apply pure_wp_throw.
+      split; eauto.
+    - pure_simp. }
+  intros [??] (head & Hhead & -> & ->); simpl.
 
-<<<<<<< variant A
   (* Struct item: [let catch_head l = ...] *)
   eapply structs_cons.
   { apply struct_let_single with (spec := catch_head_spec).
@@ -121,8 +64,8 @@ Proof.
     pure_enter.
     eapply pure_eval_match'_exn.
     - eapply pure_simp. simp. apply Hhead.
-    - intros h (t & ->). pure_simp. reflexivity.
-    - intros e (-> & ->). pure_simp. reflexivity.  }
+    - intros h (t & ->). pure_simp.
+    - intros e (-> & ->). pure_simp. }
   intros [??] (catch_head & Hcatch_head & -> & ->); simpl.
 
   (* Struct item: [let catch_head2 l = ...] *)
@@ -134,14 +77,16 @@ Proof.
      we can do better here. *)
     (* TODO: Refactor. *)
     eapply pure_eval_match'_exn with (φ' := λ a, a = #(list.head l)) (ζ := λ e, l = [] ∧ e = VXData (Loc 0) []).
-    { eapply pure_eval_data'; last done.
+    { eapply pure_eval_data_val; last done.
       simpl_evals. fold eval. eapply pure_wp_Par_conseq.
       + eapply pure_eval_app. pure_path. pure_path.
+        unfold head_spec in Hhead.
+        intros ?? <- ->. apply Hhead.
       + eapply pure_wp_ret_singleton.
       + intros; returns_eauto. cbn in *.
         eapply pure_wp_ret; subst. destruct Ha_ensures; subst.
         red in H1; subst.
-        exists [v]; tauto.
+        exists [ #v ]; tauto.
       + intros ? [ (-> & ->)| ]; cbn.
         * eapply pure_wp_throw; eauto.
         * Unshelve.
@@ -150,12 +95,11 @@ Proof.
     { intros ? ->.
       pure_match.
       pure_path. }
-    { intros ? [-> ->]. pure_match. pure_data. encode. } }
+    { intros ? [-> ->]. pure_match. pure_simp. } }
 
->>>>>>> variant B
-======= end
   intros [??] (catch_head2 & Hcatch_head2 & -> & ->); simpl.
 
   (* We have gone though all of the struct items, time to conclude. *)
-  by eapply structs_nil.
+  eapply structs_nil.
+  done.
 Qed.
