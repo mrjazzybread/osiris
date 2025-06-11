08b29e94b5e0000bfca5b5eb3411dc1eb37eef3d
Irene Yoon <euisun.irene.yoon@gmail.com>
2025-06-12 00:06:41 +0200

    Full bst proof; could not find full proof in any other branches

--- coq-osiris/examples/src/bst.ml
+++ coq-osiris/examples/src/bst.ml
@@ -2,22 +2,24 @@ type 'a bst =
   | Node of 'a bst * 'a * 'a bst
   | Leaf
 
-let rec insert x = function
-    | Leaf -> Node (Leaf, x, Leaf)
-    | Node (l, y, r) ->
+let rec insert (x, v) =
+  match v with
+  | Leaf -> Node (Leaf, x, Leaf)
+  | Node (l, y, r) ->
       if x < y then
-	 Node (insert x l, y, r)
+        Node (insert (x, l), y, r)
       else if x > y then
-	 Node (l, y, insert x r)
+        Node (l, y, insert (x, r))
       else
-	 Node (l, y, r)
+        Node (l, y, r)
 
-let rec member x = function
-    | Leaf -> false
-    | Node (l, y, r) ->
+let rec member (x, v) =
+  match v with
+  | Leaf -> false
+  | Node (l, y, r) ->
       if x < y then
-	member x l
+        member (x, l)
       else if x > y then
-	member x r
+        member (x, r)
       else
-	true
+        true
