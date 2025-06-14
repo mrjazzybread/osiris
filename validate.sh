d76ab985d8d766069c51da25fc72b036ac0045de
Remy Seassau <remy.seassau@gmail.com>
2025-06-14 02:07:50 +0100

    Add [validate.sh] script to run [coqchk].

---
+++ validate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+# Define logical paths
+LOGICAL_PATHS=(
+  "coq-osiris/_build/default/theories=osiris"
+  "coq-osiris/_build/default/examples=osiris.examples"
+)
+
+# Collect module names for each path
+MODULES=()
+
+for entry in "${LOGICAL_PATHS[@]}"; do
+  PHYS_DIR="${entry%%=*}"
+  LOGICAL_NS="${entry##*=}"
+
+  MODS=$(find "$PHYS_DIR" -name "*.vo" | sed -e "s|$PHYS_DIR/||" -e 's|\.vo$||' -e 's|/|.|g' | sed "s|^|$LOGICAL_NS.|")
+  MODULES+=($MODS)
+done
+
+# Build -R flags
+R_FLAGS=()
+for entry in "${LOGICAL_PATHS[@]}"; do
+  PHYS_DIR="${entry%%=*}"
+  LOGICAL_NS="${entry##*=}"
+  R_FLAGS+=("-R" "$PHYS_DIR" "$LOGICAL_NS")
+done
+
+echo "Running coqchk, this may take a minute or two..."
+coqchk -silent -o "${R_FLAGS[@]}" "${MODULES[@]}" 2>&1 | grep -v -E "Coq\.Floats|Coq\.Numbers|Coq\.Reals"
