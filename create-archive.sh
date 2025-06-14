42d038ea6fd66029697b6dcb636e10d8ff3aec7f
Remy Seassau <remy.seassau@gmail.com>
2025-06-14 04:29:59 +0100

    Idea: project has to be a repo for make clean to work, not yet fixed

--- create-archive.sh
+++ create-archive.sh
@@ -14,9 +14,8 @@ ARCHIVE=osiris-archive
 rm -rf $ARCHIVE $ARCHIVE.tar.gz
 
 git clone . $ARCHIVE
-
 rm -rf $ARCHIVE/.git
-rm     $ARCHIVE/.git*
+rm     $ARCHIVE/.gitlab-ci.yml
 rm     $ARCHIVE/AUTHORS.md
 rm     $ARCHIVE/TODO.md
 rm     $ARCHIVE/create-archive.sh
@@ -30,6 +29,8 @@ sed -i '/Daby-Seesaram/d' $ARCHIVE/{,coq-}osiris/dune-project
 sed -i '/Madiot/d'        $ARCHIVE/{,coq-}osiris/dune-project
 sed -i '/Seassau/d'       $ARCHIVE/{,coq-}osiris/dune-project
 sed -i '/Yoon/d'          $ARCHIVE/{,coq-}osiris/dune-project
+
+git init .
 
 $TAR cvfz $ARCHIVE.tar.gz \
   --exclude-ignore-recursive=.gitignore \
