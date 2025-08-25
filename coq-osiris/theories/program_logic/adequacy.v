da7307d0fa31018cf6a8151b7ef53343798feadf
Remy Seassau <remy.seassau@gmail.com>
2025-08-25 14:29:22 +0200

    Deprecate adequacy proof.

--- coq-osiris/theories/program_logic/adequacy.v
+++ coq-osiris/theories/program_logic/adequacy.v
@@ -14,35 +14,15 @@ Section ewp_wp.
     [EWP] is a consequence of the adequacy of [WP]. This is following the adequacy
     proof of [Hazel] (de Vilhena & Pottier) *)
 
-  Lemma ewp_imp_wp {Σ} {A X}
-    {irisGen: irisGS_gen HasNoLc (@osiris_lang A X) Σ}
-    E (m : micro A X) (Φ : outcome2 A X -> _) :
+  Lemma ewp_imp_wp {Σ}
+    {irisGen: irisGS_gen HasNoLc (@osiris_lang val exn) Σ}
+    E (m : micro val exn) (Φ : outcome2 val exn -> _) :
     EWP m @ E <| ⊥ |> {{ Φ }} -∗ WP m @ NotStuck; E {{ Φ }} : iProp Σ.
   Proof.
-    iLöb as "IH" forall (m).
-    iIntros "Hwp".
-    destruct (to_val m) as [ v         |] eqn:?.
-    (* [e] is an outcome2 (either [ret _] or [throw _]). *)
-    { rewrite ewp_unfold /ewp_pre wp_unfold /wp_pre /= Heqo.
-      destruct m; inversion Heqo; subst; eauto. }
-    rewrite ewp_unfold /ewp_pre wp_unfold /wp_pre /= Heqo.
-    ewp_case_is_handleable m ;inversion Heqo.
-    { iMod "Hwp". done. }
-    iMod "Hwp".
-    { rewrite /prot; rewrite upcl_bottom; done. }
-    intro_state. iMod ("Hwp" with "Hsi") as "[% H]".
-    iSplitL "".
-    { iPureIntro; destruct H, x. eexists nil, _,_, nil; cbn; split; eauto. }
-    iModIntro. iIntros (e2 σ2 efs step) "H£".
-    destruct step as (Hstep&->).
-    iSpecialize ("H" $! _ _ Hstep).
-    iMod "H"; iModIntro. iNext.
-    repeat iModIntro.
-    iApply step_fupdN_intro; [set_solver | ..].
-    iNext; cbn; iMod "H"; iModIntro.
-    iDestruct "H" as "[$ Hwp]"; iSplitR ""; last done.
-    iApply ("IH" with "Hwp").
-  Qed.
+  Abort.
+
+  (* This strategy no longer works, because the Iris-instantiate [WP] is not
+     able to step through forks or joins. *)
 
 End ewp_wp.
 
@@ -59,7 +39,7 @@ Section adequacy.
   (* ------------------------------------------------------------------------ *)
   (** Adequacy Theorem for [EWP] for computations. *)
 
-  Theorem ewp_adequacy m σ φ :
+  Theorem ewp_adequacy (m : micro A X) σ φ :
   (∀ `{!irisGS_gen HasNoLc (@osiris_lang A X) Σ},
     (* If [⊢ ⟨ ⊥ ⟩ impure m (λ v. ⌜φ v⌝)] holds *)
     ⊢ EWP m @ ⊤ <| ⊥ |> {{ fun o =>  ⌜ φ o ⌝ }}) →
@@ -68,45 +48,6 @@ Section adequacy.
       σ (* in any initial heap, *)
       (λ o _, φ o) (* and the returned outcome satisfies the postcondition [φ] *).
   Proof.
-    intros Hwp.
-    eapply (wp_adequacy_gen HasNoLc Σ _).
-    iIntros (??) "".
-    iMod (gen_heap_init σ) as (?) "[Hh _]".
-    iModIntro. iExists
-      (λ σ κs, gen_heap_interp σ),
-      (λ _, True%I). iFrame.
-    iApply ewp_imp_wp. iApply Hwp.
-  Qed.
-  (* In particular, instantiating
-     - [σ] with the empty heap
-     - [φ] with [match o with | O2Ret v => φ' v | O2Throw v => False]
-     Yields theorem 8.1 from the paper.
-
-     Furthermore, instantiating [m] with [eval η e] yields corollary 8.2 *)
-
-  (* To further substantiate the claim that programs proved with the empty protocol
-     cannot perform unhandled effects, consider the following corollary. *)
-  Corollary no_unhandled_effect m σ φ :
-  (∀ `{!irisGS_gen HasNoLc (@osiris_lang A X) Σ},
-    (* If [⊢ ⟨ ⊥ ⟩ impure m (λ v. ⌜φ v⌝)] holds *)
-    ⊢ EWP m @ ⊤ <| ⊥ |> {{ fun o => ⌜ φ o ⌝ }}) →
-    (* For any configuration [(t, σ')] *)
-    ∀ t σ',
-      (* reached from the starting configuration [([m], σ)]. *)
-      rtc erased_step ([m], σ) (t, σ') →
-      (* None of the computations in the threadpool are unhandled effects. *)
-      ∀ m',
-        m' ∈ t →
-        ∀ e k, m' ≠ Stop CPerf e k.
-  Proof.
-    intros [_ H]%(ewp_adequacy _ σ).
-    intros t σ' S m' I e k Heq. subst m'.
-    specialize (H t σ' _ eq_refl S I).
-    destruct H.
-    - destruct H. discriminate.
-    - destruct H as (? & ? & ? & ? & Hstep).
-      inversion Hstep as [Hstep' ->].
-      inversion Hstep'.
-  Qed.
+  Abort.
 
 End adequacy.
