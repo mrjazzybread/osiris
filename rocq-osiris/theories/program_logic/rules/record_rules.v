dfa5348d170f841990c585891b335576052c541b
Remy Seassau <remy.seassau@gmail.com>
2026-05-12 15:21:47 +0200

    Finish record access rule

--- rocq-osiris/theories/program_logic/rules/record_rules.v
+++ rocq-osiris/theories/program_logic/rules/record_rules.v
@@ -92,7 +92,7 @@ Section records_reasoning.
     λ i xs, if decide (i < 0) then None else
       let fix go (i : nat) (τ : types) {struct τ} : option Type :=
         match τ, i with
-        | Tbase X, 1%nat => Some X
+        | Tbase X, 0%nat => Some X
         | Tbase _, _ => None
         | type_nel.Tcons X _, 0%nat => Some X
         | type_nel.Tcons _ τ, (S i0) => go i0 τ
@@ -104,7 +104,7 @@ Section records_reasoning.
     λ i xs, if decide (i < 0) then ()%type else
       let fix go (i : nat) (τ : types) {struct τ} : Type :=
         match τ, i with
-        | Tbase X, 1%nat => X
+        | Tbase X, 0%nat => X
         | Tbase _, _ => unit
         | type_nel.Tcons X _, 0%nat => X
         | type_nel.Tcons _ τ, (S i0) => go i0 τ
@@ -115,7 +115,7 @@ Section records_reasoning.
   Definition types_go : nat -> types -> Type :=
     fix go (i : nat) (τ : types) {struct τ} : Type :=
       match τ, i with
-      | Tbase X, 1%nat => X
+      | Tbase X, 0%nat => X
       | Tbase _, _ => unit
       | type_nel.Tcons X _, 0%nat => X
       | type_nel.Tcons _ τ', S i0 => go i0 τ'
@@ -123,7 +123,7 @@ Section records_reasoning.
 
   Fixpoint tau_lookup_go (τ : types) (i : nat) : τ -> types_go i τ :=
     match τ, i return τ -> types_go i τ with
-    | Tbase X, 1%nat => fun xs => xs
+    | Tbase X, 0%nat => fun xs => xs
     | Tbase _, _ => fun _ => tt
     | type_nel.Tcons X _, 0%nat => fun xs => fst xs
     | type_nel.Tcons _ τ', S i0 => fun xs => tau_lookup_go τ' i0 (snd xs)
@@ -145,15 +145,58 @@ Section records_reasoning.
     case_decide. { apply _. }
     generalize (Z.to_nat f).
     induction τ; intros n.
-    - destruct n as [|[|]]; apply _.
+    - destruct n; apply _.
     - destruct n.
-      + destruct τ0; apply _.
+      + apply _.
       + apply IHτ.
+  Defined.
+
+  Global Instance inhabited_val : Inhabited val :=
+    { inhabitant := VUnit }.
+
+  Local Instance Encode_types_aux {τ} n : Encode (types_go n τ).
+  Proof.
+    revert n.
+    induction τ; intros n.
+    - simpl. destruct n; apply _.
+    - simpl. destruct n; first apply _.
+      apply IHτ.
+  Defined.
+
+  Local Lemma lookup_total_to_vals_aux {τ : types} (xs : τ) n :
+    (to_vals xs) !!! n = #(tau_lookup_go τ n xs).
+  Proof.
+    revert xs n.
+    induction τ as [X H | X H b IH]; intros xs n.
+    - rewrite tapp_bind.
+      destruct n as [|n'] eqn:Hnat; first reflexivity.
+      rewrite (list.lookup_total_cons_ne_0 _ _ _ _); last lia.
+      rewrite list.lookup_total_nil. reflexivity.
+    - pose proof (Tcons_inv b xs) as [x [xs' ->]].
+      simpl; rewrite tapp_bind.
+      destruct n as [|n'] eqn:Hnat; first reflexivity.
+      rewrite (list.lookup_total_cons_ne_0 _ _ _ _); last lia.
+      apply IH.
+  Qed.
+
+  Lemma lookup_total_to_vals {τ : types} (xs : τ) (f : Z) :
+    (to_vals xs) !!! f = #(xs !!τ f).
+  Proof.
+    unfold "!!τ", τ_lookup_total, encode_types_lookup, lookup_total, types_lookup_total, listz_lookup_total.
+    case_decide as Hlt. { reflexivity. }
+    generalize (Z.to_nat f) as n; intros n; clear dependent f.
+    apply lookup_total_to_vals_aux.
   Qed.
 
   Global Instance notval_block : NotVal (mut_tag * list loc) := {}.
 
+  Global Instance inhabited_loc : Inhabited loc.
+  Proof.
+    do 2 constructor. apply Z.inhabited.(inhabitant).
+  Qed.
+
   Lemma imp_ERecordAccess {τ : types} {ζ} f {Φ : (τ !!! f) → iProp Σ} r ls e :
+    ⌜valid f ls⌝ -∗
     ▷ isBlockLocs r ls -∗
     impure E (eval η e) Ψ ζ (λ (r' : record), ⌜r' = r⌝) -∗
     (∃ dq t (xs : τ),
@@ -161,19 +204,40 @@ Section records_reasoning.
       ▷ (ownRecord r dq t xs -∗ Φ (xs !!τ f))) -∗
     impure E (eval η (ERecordAccess e f)) Ψ ζ Φ.
   Proof.
-    iIntros "#Hblock He P". simpl_eval.
+    iIntros "%Hvalid #Hblock He P". simpl_eval.
     iApply (imp_bind with "[He]").
     { iApply (imp_as_record with "He"). }
     iIntros (?) "->".
     iDestruct "P" as
         "(%dq & %t & %xs & P)".
     rewrite <- (bi.later_sep (ownRecord _ _ _ _)).
+    iCombine "Hblock P" as "P".
 
 
     iApply (imp_bind (A1:=(mut_tag * list loc)) with "[P]").
     { iApply (imp_load_block_ghost' with "Hblock P"). }
-    iIntros ((? & ?)) "(-> & Hown & HΦ) /=".
+    iIntros ((? & ?)) "(-> & #Hblock' & Hown & HΦ) /=".
+    iClear "Hblock".
 
-    Admitted.
+    iDestruct "Hown" as "(%ls' & #Hblock & Htag & Hxs)".
+    iPoseProof (isBlockLocs_valid with "Hblock' Hblock") as "->".
+    iPoseProof (isBlockLocs_length with "Hblock") as "%Hlen".
+    iPoseProof (big_sepLZ2_length with "Hxs") as "%Hlen_xs".
+    rewrite (list_lookup_lookup_total_valid ls f Hvalid).
+
+    iPoseProof (big_sepLZ2_lookup_acc _ ls (to_vals xs) f with "Hxs") as "(Hx & Hxs)".
+    { apply list_lookup_lookup_total_valid. assumption. }
+    { instantiate (1:= #(xs !!τ f)).
+      transitivity (@Some val ((to_vals xs) !!! f)).
+      - apply list_lookup_lookup_total_valid.
+        rewrite <- Hlen_xs. assumption.
+      - f_equal.
+        apply lookup_total_to_vals. }
+    iApply (imp_load' with "Hx").
+    iIntros "!> Hx".
+    iApply "HΦ".
+    iExists ls. iFrame "∗#".
+    iApply ("Hxs" with "Hx").
+  Qed.
 
 End records_reasoning.
