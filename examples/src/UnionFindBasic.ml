dd99208f999e3802dd509d985135ab1ca0257915
Remy Seassau <remy.seassau@gmail.com>
2026-06-04 15:21:26 +0200

    Modify union-find to not use inline records (not yet supported)

--- examples/src/UnionFindBasic.ml
+++ examples/src/UnionFindBasic.ml
@@ -38,9 +38,15 @@ type rank =
    - only [union] and [merge] allocate a new edge (a [Link] content).
    [find], [get], [set], [eq] allocate no memory. *)
 
-type 'a content =
-| Link of { mutable parent : 'a elem }
-| Root of { mutable rank : rank; mutable value : 'a }
+type 'a link =
+  { mutable parent : 'a elem }
+
+and 'a root =
+  { mutable rank : rank; mutable value : 'a }
+
+and 'a content =
+| Link of 'a link
+| Root of 'a root
 
 (* The type ['a elem] represents a vertex in the union-find data structure. *)
 
